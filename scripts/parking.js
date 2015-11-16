870faeef79484e7c8c2a6bc08e292ff8341e87bd
Joseph Garrone <Joseph Garrone>
2015-11-16 16:52:16 +1000

    Changed some of the parking names

--- scripts/parking.js
+++ scripts/parking.js
@@ -36,15 +36,15 @@ module.exports = function (robot) {
         "p11 l3": "Conifer L3 (Students)",
         "p10": "UQ Centre",
         "p9": "Boatshed Open",
-        "p8 l1": "Boatshed Bot",
+        "p8 l1": "Boatshed Bottom",
         "p8 l2": "Boatshed Top",
         "p7": "Dustbowl",
         "p6": "BSL Short Term",
         "p5": "P5",
-        "p4": "Multi Level",
-        "p3": "Multi Level",
-        "p2": "P2",
-        "p1": "P1"
+        "p4": "Multi Level 3",
+        "p3": "Multi Level 2",
+        "p2": "Multi Level 1",
+        "p1": "Warehouse"
       };
       
       var response = ">Available parking at the University of Queensland\r\n";
