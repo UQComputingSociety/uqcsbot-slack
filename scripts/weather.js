4e212ce286fe8ee4e47f79208487c67d57fe8991
JosephGarrone <josephgarrone@gmail.com>
2015-11-16 17:26:56 +1000

    Added !weather

---
+++ scripts/weather.js
@@ -0,0 +1,33 @@
+// Description
+//   Gets the weather for st lucia by default or a specified location
+// 
+// Commands:
+//   !weather <location (opt)> - Gets the weather for a location (Defaults to Brisbane) 
+// 
+
+var APPID = "6854f325de5158ff8020c1ea06333234";
+
+module.exports = function (robot) {
+  robot.hear(/^!weather ?(.+)?/i, function (res) {
+	  var location = res.match[1] || "Brisbane";
+	  robot.getWeather(location, res)
+  });
+  
+  robot.getWeather = function(location, res) {
+	  robot.http("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=" + APPID).get()
+	  (function(err, resp, body) {
+		 var json = JSON.parse(body);
+		 
+		 if (json.cod.toString().trim() == "200") {
+			 var temp = Math.round(json.main.temp - 273, 2);
+			 var humidity = json.main.humidity;
+			 var min = Math.round(json.main.temp_min - 273, 2);
+			 var max = Math.round(json.main.temp_max - 273, 2);
+			 var weather = json.weather[0].description;
+			 res.send(">*" + json.name + "* is *" + temp + "°C* (min: *" + min + "°C*, max: *" + max + "°C*) with humidity at *" + humidity + "%* and *" + weather + "*");
+		 } else {
+			 res.send(">Can't find " + location);
+		 }
+	  });
+  }
+}
