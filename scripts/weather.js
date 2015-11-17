5db06d60e7794d1f3afa639b39d8a7a2d11b45e4
Matthew Lake <mgtlake@outlook.com>
2015-11-17 23:22:00 +1000

    change all js files to use tabs to allow users to customise indents

--- scripts/weather.js
+++ scripts/weather.js
@@ -7,29 +7,29 @@
 
 module.exports = function (robot) {
   robot.hear(/^!weather ?(.+)?/i, function (res) {
-	  var location = res.match[1] || "Brisbane";
-	  robot.getWeather(location, res)
+      var location = res.match[1] || "Brisbane";
+      robot.getWeather(location, res)
   });
   
   robot.getWeather = function(location, res) {
-	  var query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '") and u="c"';
-	  var url = "https://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(query) + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-	  
-	  robot.http(url).get()
-	  (function(err, resp, body) {
-		 var json = JSON.parse(body);
-		 var data = json.query.results.channel;
-		 var today = data.item.forecast[0];
-		 if (err == null) {
-			 var temp = Math.round(data.item.condition.temp * 100) / 100;
-			 var humidity = data.atmosphere.humidity;
-			 var min = Math.round(today.low * 100) / 100;
-			 var max = Math.round(today.high * 100) / 100;
-			 var weather = data.item.condition.text;
-			 res.send(">*" + data.location.city + ", " + data.location.country + "* is *" + temp + "°C* (min: *" + min + "°C*, max: *" + max + "°C*) with humidity at *" + humidity + "%* and condition *" + weather + "*");
-		 } else {
-			 res.send(">Can't find " + location);
-		 }
-	  });
+      var query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '") and u="c"';
+      var url = "https://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(query) + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+      robot.http(url).get()
+      (function(err, resp, body) {
+         var json = JSON.parse(body);
+         var data = json.query.results.channel;
+         var today = data.item.forecast[0];
+         if (err == null) {
+             var temp = Math.round(data.item.condition.temp * 100) / 100;
+             var humidity = data.atmosphere.humidity;
+             var min = Math.round(today.low * 100) / 100;
+             var max = Math.round(today.high * 100) / 100;
+             var weather = data.item.condition.text;
+             res.send(">*" + data.location.city + ", " + data.location.country + "* is *" + temp + "°C* (min: *" + min + "°C*, max: *" + max + "°C*) with humidity at *" + humidity + "%* and condition *" + weather + "*");
+         } else {
+             res.send(">Can't find " + location);
+         }
+      });
   }
 }
--- scripts/welcome.js
+++ scripts/welcome.js
@@ -3,14 +3,14 @@
 // 
 
 var welcomeMessages = [
-  "Hey there! Welcome to the UQCS slack!",
-  "This is the first time I've seen you, so you're probably new here",
-  "I'm UQCSbot, your friendly (open source) robot helper",
-  "Introduce yourself on #intros, join some channels", 
-  "We've got a bunch of generic channels (e.g. #banter, #games, #projects) along with some subject-specific ones",
-  "Your friendly admins are @trm, @svict4, @gricey, @rachcatch, @ainsleynand",
-  "Type \"help\" here, or \"uqcsbot help\" anywhere else to find out what I can do.",
-  "and again, welcome :)"
+	"Hey there! Welcome to the UQCS slack!",
+	"This is the first time I've seen you, so you're probably new here",
+	"I'm UQCSbot, your friendly (open source) robot helper",
+	"Introduce yourself on #intros, join some channels", 
+	"We've got a bunch of generic channels (e.g. #banter, #games, #projects) along with some subject-specific ones",
+	"Your friendly admins are @trm, @svict4, @gricey, @rachcatch, @ainsleynand",
+	"Type \"help\" here, or \"uqcsbot help\" anywhere else to find out what I can do.",
+	"and again, welcome :)"
 ];
 
 //The time between each individual welcome message send
@@ -18,14 +18,14 @@ var messageTime = 2500;
 
 
 module.exports = function(robot){
-  robot.enter(function(res){
-    if(res.message.room == "general"){
-      res.send("Welcome, " + res.message.user.name + "!");
-      welcomeMessages.forEach(function(msg, i) {
-        setTimeout(function(){
-          robot.send({room: res.message.user.name}, msg);
-        }, messageTime*i);
-      });
-    }
-  });
+	robot.enter(function(res){
+		if(res.message.room == "general"){
+			res.send("Welcome, " + res.message.user.name + "!");
+			welcomeMessages.forEach(function(msg, i) {
+				setTimeout(function(){
+					robot.send({room: res.message.user.name}, msg);
+				}, messageTime*i);
+			});
+		}
+	});
 }
