41a07b3406a2848f327ac87e662746d91a2e13d9
Simon Victory <email@simonvictory.com>
2015-11-23 11:14:18 +1000

    removed unnecessary examples, timeout is still behaving weird

--- scripts/conversation.js
+++ scripts/conversation.js
@@ -12,38 +12,14 @@ module.exports = function (robot) {
     var switchBoard = new Conversation(robot);
 
     robot.respond(/what is love?/, function (msg) {
-        var dialog = switchBoard.startDialog(msg, 5000);
-        msg2.reply('Baby don\'t hurt me');
+        var dialog = switchBoard.startDialog(msg);
+        msg.reply('Baby don\'t hurt me');
         
         dialog.timeout = function (msg2) {
-            msg.reply('C-C-C-C-Combo Breaker!');
+            msg2.reply('C-C-C-C-Combo Breaker!');
         }
-        dialog.addChoice(/don't hurt me/i, function (msg2) {
+        dialog.addChoice(/don't hurt me/i, function (msg3) {
             msg3.reply('no more');
-        });
-    });
-
-    robot.respond(/jump/, function (msg) {
-        var dialog = switchBoard.startDialog(msg);
-        msg.reply('Sure, How many times?');
-
-        dialog.addChoice(/([0-9]+)/i, function (msg2) {
-            var times = parseInt(msg2.match[1], 10);
-            for (var i = 0; i < times; i++) {
-                msg.emote("Jumps"); //We can use the original message too.
-            }
-        });
-    });
-
-
-  robot.respond(/.*the mission/, function (msg) {
-        msg.reply('Your have 5 seconds to accept your mission');
-        var dialog = switchBoard.startDialog(msg, 5000); //5 Second timeout
-        dialog.timeout = function (msg2) {
-            msg2.reply('Boom');
-        }
-        dialog.addChoice(/yes/i, function (msg2) {
-            msg2.reply('Great! Here are the details...');
         });
     });
 };
