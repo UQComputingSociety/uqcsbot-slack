cd2b88c7d5f283dcc3bf56acfe634121264f1b67
Simon Victory <email@simonvictory.com>
2015-11-23 11:05:20 +1000

    Fixed more silly syntax errors

--- scripts/computers.js
+++ scripts/computers.js
@@ -14,20 +14,27 @@ module.exports = function (robot) {
 			var computers = [];
 
 			// scrap the data
-			$('table.chart').children().children().each(function (index, element) {
-				var name  = $($(element).children()[0]).text().trim();
-				var percent = $($(element).children()[1]).text().trim()
+			$('table.chart').children().each(function (index, element) {
+				var name  = $($(element).children()[0]).children().text().trim();
+				var percent = $($($(element).children()[1]).children()).text().trim();
 				var freeText = $($(element).children()[2]).text().trim();
 				
 				var free = freeText.split(' ')[0];
-				var freeOf = freeText.split(' ')[3]
+				var freeOf = freeText.split(' ')[3];
+				
+				/* res.send($(element).children());
+				res.send('1 ' + name);
+				res.send('2 ' + percent);
+				res.send('3 ' + freeText);
+				res.send('4 ' + free);
+				res.send('5 ' + freeOf); */
 				
 				//console.log(name + ' is ' + percent + ' free. Of which ' + free + ' is free of ' + freeOf);
 				computers.push([name, percent, free, freeOf]);	
 			});
 
 			var response = ">Available computers at St Lucia";
-
+			
 			for (var i = 0; i < computers.length; i++) {
 				var name_r = computers[i][0];
 				var percent_r = computers[i][1];
@@ -35,10 +42,11 @@ module.exports = function (robot) {
 				var freeOf_r = computers[i][3];
 				
 				var realPercent = parseInt(Math.round(parseInt(percent_r.substring(0, percent_r.length-1)) / 10) * 10 / 2 / 10);
+				
 				response += ">_" + name_r + "_ *" + percent_r + "* " + (Array(5 - realPercent + 1).join(":no_entry:")) + (Array(realPercent + 1).join(":thumbsup:")) + " " + free_r + " computers free" + "\r\n";
 			}
 			
-		res.send(resposne);
+		res.send(response);
 		});
 	});
 }
