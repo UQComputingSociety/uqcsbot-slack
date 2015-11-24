5f0ccc65098938f75cf718d983238e8c829afcbd
Matthew Lake <mgtlake@outlook.com>
2015-11-24 21:45:08 +1000

    fix identation

--- scripts/weather.js
+++ scripts/weather.js
@@ -15,24 +15,24 @@ module.exports = function (robot) {
 		var query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '") and u="c"';
 		var url = "https://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(query) + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
-	robot.http(url).get()
-		(function(err, resp, body) {
-			var json = JSON.parse(body);
-			var data = json.query.results.channel;
-			var today = data.item.forecast[0];
-			if (err == null) {
-				var temp = Math.round(data.item.condition.temp * 100) / 100;
-				var humidity = data.atmosphere.humidity;
-				var min = Math.round(today.low * 100) / 100;
-				var max = Math.round(today.high * 100) / 100;
-				var weather = data.item.condition;
-				res.send(">" + robot.getWeatherEmoji(parseInt(weather.code)) + " *" + data.location.city + ", " +
-					data.location.country + "* is *" + temp + "°C* (min: *" + min+ "°C*, max: *" + max +
-					"°C*) with humidity at *" + humidity + "%* and condition *" + weather.text + "*");
-			} else {
-				res.send(">Can't find " + location);
-			}
-		});
+		robot.http(url).get()
+			(function(err, resp, body) {
+				var json = JSON.parse(body);
+				var data = json.query.results.channel;
+				var today = data.item.forecast[0];
+				if (err == null) {
+					var temp = Math.round(data.item.condition.temp * 100) / 100;
+					var humidity = data.atmosphere.humidity;
+					var min = Math.round(today.low * 100) / 100;
+					var max = Math.round(today.high * 100) / 100;
+					var weather = data.item.condition;
+					res.send(">" + robot.getWeatherEmoji(parseInt(weather.code)) + " *" + data.location.city + ", " +
+						data.location.country + "* is *" + temp + "°C* (min: *" + min+ "°C*, max: *" + max +
+						"°C*) with humidity at *" + humidity + "%* and condition *" + weather.text + "*");
+				} else {
+					res.send(">Can't find " + location);
+				}
+			});
 	}
 
 	robot.getWeatherEmoji = function(code) {
