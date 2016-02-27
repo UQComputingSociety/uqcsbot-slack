91bf92456f8757bf7a99f9013aea10edc61247bc
Tom Manderson <TRManderson@gmail.com>
2016-02-27 11:08:23 +1000

    Revert "Update welcome.js"
    Evan, don't be a dickhead
    
    This reverts commit 4da76ac90906bb156e630a5b0d22ad410ce2039f.

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -8,7 +8,7 @@ var welcomeMessages = [
 	"I'm UQCSbot, your friendly (open source) robot helper",
 	"Introduce yourself on #intros, join some channels", 
 	"We've got a bunch of generic channels (e.g. #banter, #games, #projects) along with some subject-specific ones",
-	"Your friendly admins are @wisebaldone I run this joint now",
+	"Your friendly admins are @trm, @svict4, @gricey, @rachcatch, @ainsleynand",
 	"Type \"help\" here, or \"uqcsbot help\" anywhere else to find out what I can do.",
 	"and again, welcome :)"
 ];
@@ -19,7 +19,7 @@ var messageTime = 2500;
 module.exports = function(robot){
 	robot.enter(function(res){
 		if(res.message.room == "general"){
-			res.send("Go Away, " + res.message.user.name + "! :horse:");
+			res.send("Welcome, " + res.message.user.name + "!");
 			welcomeMessages.forEach(function(msg, i) {
 				setTimeout(function(){
 					robot.send({room: res.message.user.name}, msg);
