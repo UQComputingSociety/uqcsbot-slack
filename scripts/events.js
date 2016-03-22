9209641b3065df643ca666568ca037b315e3ef55
lewisjb <lbobbermen@bigpond.com>
2016-03-22 13:20:41 +1000

    Fixed spelling misake and add 2 week period for events

--- scripts/events.js
+++ scripts/events.js
@@ -1,6 +1,6 @@
 // Description
-//   Parses the UQCS calender (https://calendar.google.com/calendar/ical/q3n3pce86072n9knt3pt65fhio%40group.calendar.google.com/public/basic.ics)
-// 
+//   Parses the UQCS calendar (https://calendar.google.com/calendar/ical/q3n3pce86072n9knt3pt65fhio%40group.calendar.google.com/public/basic.ics)
+//
 // Commands:
 //   !events - List UQCS Events
 //
@@ -11,23 +11,24 @@ module.exports = function (robot) {
 		var months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
 		ical.fromURL('https://calendar.google.com/calendar/ical/q3n3pce86072n9knt3pt65fhio%40group.calendar.google.com/public/basic.ics', {}, function(err, data) {
 			var now = new Date().getTime();
-			var arr = []
+			var arr = [];
 			for(var k in data) {
-				if(data[k].end.getTime() > now) {
+				// Hasn't finished, and it starts in at least two weeks
+				if(data[k].end.getTime() > now && data[k].start.getTime() < now + 1000*60*60*24*14) {
 					arr.push(data[k]);
 				}
 			}
 			arr.sort(function(a,b) { return a.start.getTime()-b.start.getTime(); });
-			var ret = "";
+			var ret = "Events in the *next two weeks*. For a list of all visit: https://uqcs.org.au/calendar.html";
 			for(var k in arr) {
 				var ev = arr[k];
 				if(ev.location == "") {ev.location = "Unkown location";}
-				ret += "*" + months[ev.start.getMonth()] + " " + ev.start.getDate() + " " + ev.start.getHours() + ":" + 
+				ret += "\r\n*" + months[ev.start.getMonth()] + " " + ev.start.getDate() + " " + ev.start.getHours() + ":" +
 					(ev.start.getMinutes() < 10 ? "0" : "") + ev.start.getMinutes() +
 					" - " + months[ev.end.getMonth()] + " " + ev.end.getDate() + " " + ev.end.getHours() + ":" +
 					(ev.end.getMinutes() < 10 ? "0" : "") + ev.end.getMinutes() + "*" +
 					" - _" + ev.location + "_" +
-					": `" + ev.summary + "`\r\n";
+					": `" + ev.summary + "`";
 			}
 			res.send(ret);
 		});
