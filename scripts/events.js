e1e4d7b16f157db52b3b61657793a4459bb479e9
lewisjb <lbobbermen@bigpond.com>
2016-03-31 14:17:34 +1000

    Cleaned up events.js

--- scripts/events.js
+++ scripts/events.js
@@ -9,66 +9,63 @@ var ical = require('ical');
 module.exports = function (robot) {
 	robot.respond(/!?events ?(full|[1-9][0-9]*)? ?(weeks?)?/i, function (res) {
 		var months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-		if (res.match[1] === undefined) { res.match[1] = ""; }
-		if (res.match[2] === undefined) { res.match[2] = ""; }
+		var no_result = ["There doesn't appear to be any events in the next two weeks...",
+				 "There doesn't appear to be any events in the next *<n>* weeks...",
+				 "There doesn't appear to be any upcoming events...",
+				 "There doesn't appear to be any upcoming events..."];
+		var header = ["Events in the *next two weeks*",
+			      "Events in the *next _<n>_ weeks*",
+			      "The next <n> events",
+			      "List of *all* upcoming events"];
+		var type = 0; // Two weeks
+		if (res.match[1] !== undefined) {
+			if (res.match[1] === "full") {
+				type = 3; // All events
+			} else if (res.match[2] === undefined) {
+				type = 2; // n events
+			} else {
+				type = 1; // n weeks
+			}
+		}
 		ical.fromURL('https://calendar.google.com/calendar/ical/q3n3pce86072n9knt3pt65fhio%40group.calendar.google.com/public/basic.ics', {}, function(err, data) {
 			var now = new Date().getTime();
 			var week = 1000*60*60*24*7;
 			var arr = [];
-			// !events
-			if (res.match[1] === ""){
-				for(var k in data) {
-					// Hasn't finished, and it starts in at least two weeks
-					if(data[k].end.getTime() > now && data[k].start.getTime() < now + week*2) {
+			var n = +res.match[1];
+			for(var k in data) {
+				if(n === 0) { break; }
+				switch(type) {
+					case 0:
+						if(data[k].end.getTime() > now && data[k].start.getTime() < now + week*2) {
+							arr.push(data[k]);
+						}
+						break;
+					case 1:
+						if(data[k].end.getTime() > now && data[k].start.getTime() < now + week*n) {
+							arr.push(data[k]);
+						}
+						break;
+					case 2:
 						arr.push(data[k]);
-					}
-				}
-			// !events full
-			} else if (res.match[1] === "full"){
-				for(var k in data){
-					if(data[k].end.getTime() > now){
-						arr.push(data[k]);
-					}
-				}
-			// !events <n> weeks
-			} else if (res.match[2] !== "") {
-				var n = +res.match[1];
-				for(var k in data) {
-					if(data[k].end.getTime() > now && data[k].start.getTime() < now + week*n) {
-						arr.push(data[k]);
-					}
-				}
-			// !events <n>
-			} else {
-				var n = +res.match[1];
-				for(var k in data) {
-					if(n === 0) { break; }
-					arr.push(data[k]);
-					n--;
+						n--;
+						break;
+					case 3:
+						if(data[k].end.getTime() > now){
+							arr.push(data[k]);
+						}
+						break;
 				}
 			}
 			var ret = "";
 			if(arr.length === 0) {
-				if (res.match[1] === "") {
-					ret += "_There doesn't appear to be any events in the next two weeks..._\r\n";
-				} else if (res.match[2] !== "") {
-					ret += "_There doesn't appear to be any events in the next *" + res.match[1] + "* weeks..._\r\n";
-				} else {
-					ret += "_There doesn't appear to be any upcoming events..._\r\n";
-				}
-				ret += "For a full list of events, visit: https://uqcs.org.au/calendar.html\r\n";
+				ret += "_";
+				ret += no_result[type].replace("<n>", res.match[1]);
+				ret += "_\r\nFor a full list of events, visit: https://uqcs.org.au/calendar.html\r\n";
 			}
 			else {
 				arr.sort(function(a,b) { return a.start.getTime()-b.start.getTime(); });
-				if(res.match[1] === ""){
-					ret += "Events in the *next two weeks*. For a list of all events, visit: https://uqcs.org.au/calendar.html\r\n";
-				} else if (res.match[2] !== "") {
-					ret += "Events in the *next _" + res.match[1] + "_ week" + (res.match[1] !== "1" ? "s" : "") + "*. For a list of all events, visit: https://uqcs.org.au/calendar.html\r\n";
-				} else if (res.match[1] !== "full"){
-					ret += "The next " + res.match[1] +" events. For a list of all events, visit: https://uqcs.org.au/calendar.html\r\n";
-				} else {
-					ret += "List of all upcoming events. For a list of events (including previous ones), visit: https://uqcs.org.ay/calendar.html\r\n";
-				}
+				ret += header[type].replace("<n>", res.match[1]);
+				ret += "\r\n";
 				for(var k in arr) {
 					var ev = arr[k];
 					if(ev.location == "") {ev.location = "TBA";}
