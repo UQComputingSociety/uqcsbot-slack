e1018c0083f01232cb22a3461b56188fbbbfcc67
Tom Manderson <me@trm.io>
2016-04-12 17:19:26 +1000

    Totally untested attempt at fixing wakie.js

--- scripts/wakie.js
+++ scripts/wakie.js
@@ -6,14 +6,19 @@ var HubotCron = require('hubot-cronjob');
 
 module.exports = function(robot) {
 	var fn, pattern, timezone;
-	pattern = '0 17 * * *'; // Daily at 5PM
+	pattern = '30 17 * * *'; // Daily at 5:30PM
 	timezone = 'Australia/Brisbane';
 
 	fn = function() {
 		var general = robot.adapter.client.getChannelGroupOrDMByName("general");
 		var victim = general.members[Math.floor(Math.random() * general.members.length)];
+		robot.http("https://slack.com/api/users.info").header('Accept', 'application/json').post({
+			'token': process.env.HUBOT_SLACK_TOKEN,
+			'user': victim,
+		})(function(err, resp, body){
+			return robot.messageRoom("general", "Hey @" + JSON.parse(body).user.name + "! Tell us about something cool you are working on!");
+		});
 
-		return robot.messageRoom("general", "Hey @" + victim.name + "! Tell us about something cool you are working on!");
 	};
 	return new HubotCron(pattern, timezone, fn);
 };
