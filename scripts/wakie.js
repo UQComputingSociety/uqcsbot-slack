7c599f12bf5f4402131c37f674c396bcbe579c99
Lewis B <lewisjb@users.noreply.github.com>
2016-04-17 18:04:23 +1000

    Fixed my mistake of left-over bracket
    * Replaced HTTP API request with <@U123>
    
    * Removed left-over bracket
    
    * Removed left-over bracket

--- scripts/wakie.js
+++ scripts/wakie.js
@@ -13,8 +13,6 @@ module.exports = function(robot) {
 		var general = robot.adapter.client.getChannelGroupOrDMByName("general");
 		var victim = general.members[Math.floor(Math.random() * general.members.length)];
 		return robot.messageRoom("general", "Hey <@" + victim + ">! Tell us about something cool you are working on!");
-            );
-
 	};
 	return new HubotCron(pattern, timezone, fn);
 };
