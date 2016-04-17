d048a5d1b4bdefd591b144619babbaf0be9a1c05
lewisjb <lbobbermen@bigpond.com>
2016-04-17 17:48:55 +1000

    Replaced HTTP API request with <@U123>

--- scripts/wakie.js
+++ scripts/wakie.js
@@ -12,12 +12,7 @@ module.exports = function(robot) {
 	fn = function() {
 		var general = robot.adapter.client.getChannelGroupOrDMByName("general");
 		var victim = general.members[Math.floor(Math.random() * general.members.length)];
-		robot.http("https://slack.com/api/users.info?token="
-            + process.env.HUBOT_SLACK_TOKEN
-            + "&user=" + victim).get()(
-                function(err, resp, body){
-                    return robot.messageRoom("general", "Hey @" + JSON.parse(body).user.name + "! Tell us about something cool you are working on!");
-                }
+		return robot.messageRoom("general", "Hey <@" + victim + ">! Tell us about something cool you are working on!");
             );
 
 	};
