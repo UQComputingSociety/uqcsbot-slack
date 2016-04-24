6ed486ba1d6d387e8a34e570a19a9bc2d22e3de8
Simon Victory <email@simonvictory.com>
2016-04-24 15:25:20 +1000

    Added in address Fixed #46 for unsupported postcodes

--- scripts/fuel.js
+++ scripts/fuel.js
@@ -1,9 +1,8 @@
 // Description:
-//   Gets the cheapest place to get fuel
-//
+//   Finds the cheapest local place to get fuel
 //
 // Commands:
-//   !fuel 'potscode' - tells you if you should fill up and finds the best nearby servo
+//   !`fuel` `potscode` - tells you if you should fill up and finds the best nearby servo
 //
 
 var request = require('request');
@@ -21,9 +20,9 @@ module.exports = function(robot) {
         form: { 'location': location, 'fuel-type': 'Unleaded' }
       },
       function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var $ = cheerio.load(body);
-          var response = '';
+        var $ = cheerio.load(body);
+        var response = '';
+        if ((!error) && (response.statusCode == 200) && !$('div.ajax-content p').hasClass('intro')) {
 
           //bop it, push it, twist it, scrape it
           var avg_price = $('.price strong').html();
@@ -31,24 +30,26 @@ module.exports = function(robot) {
           var commentary = $('.commentary p').html();
           var best_price = $('.fair-fuel-results tbody tr th').html();
           var best_place = $('.fair-fuel-results tbody tr:nth-child(1) td:nth-child(2)').text();
-          var best_address = $('.fair-fuel-results tbody tr:nth-child(1) td:nth-child(3)').text();
+          var best_address = $('.fair-fuel-results tbody tr:nth-child(1) td:nth-child(3)').first().contents().filter(function() {
+              return this.type === 'text';
+          }).text();
 
           if (good_bad == 'is-bad') {
             response += '>:x::fuelpump: Average price in ' + location
                      + ' is bad at: ' + avg_price + ' cents p/L\r\n';
           } else {
             response += '>:white_check_mark::fuelpump: Average price in ' + location
-                     + ' is good at ' + avg_price + ' cents p/L\r\n';
+                     + ' is good/fair at ' + avg_price + ' cents p/L\r\n';
           }
-          response += '>' + commentary + '\r\n';
+          response += '>:bulb: ' + commentary + '\r\n';
 
-          if (!($('.fair-fuel-results').hasClass('regional'))) {
-            response += '>For ' + best_price + ' cents, ' + best_place + ' has the best price in your area\r\n';
+          if ( !($('.fair-fuel-results').hasClass('regional')) || !(best_price == null) ) {
+            response += '>For ' + best_price + ' cents, ' + best_place + ' at ' + best_address + ' has the best price in your area\r\n';
           } else {
-            response += ">Unable to provide specific fuel station price data for your post code\r\n"
+            response += ">Unable to provide specific fuel station price data for your postcode\r\n"
           }
         } else {
-          res.send('There was an error getting prices. Complain to the ACCC\r\n');
+          response += ">No fuel data for this postcode and fueltype, or an error occured.\r\n";
         }
         res.send(response);
       }
