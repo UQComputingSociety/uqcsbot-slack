cd814b8f300e71d06894dd6a31a4888bb4b809f6
lewisjb <lbobbermen@bigpond.com>
2016-04-28 13:28:31 +1000

    Added 100*n member notification

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,12 +1,12 @@
 // Description
 //   Welcomes new users to UQCS Slack
-// 
+//
 
 var welcomeMessages = [
 	"Hey there! Welcome to the UQCS slack!",
 	"This is the first time I've seen you, so you're probably new here",
 	"I'm UQCSbot, your friendly (open source) robot helper",
-	"Introduce yourself on #intros, join some channels", 
+	"Introduce yourself on #intros, join some channels",
 	"We've got a bunch of generic channels (e.g. #banter, #games, #projects) along with some subject-specific ones",
 	"Your friendly admins are @trm, @svict4, @gricey, @rachcatch, @ainsleynand, @dmarj97",
 	"Type \"help\" here, or \"uqcsbot help\" anywhere else to find out what I can do.",
@@ -20,6 +20,10 @@ module.exports = function(robot){
 	robot.enter(function(res){
 		if(res.message.room == "general"){
 			res.send("Welcome, " + res.message.user.name + "!");
+			members = robot.adapter.client.getChannelOrDMByName("general").length;
+			if(members % 100 == 0) {
+				res.send(":tada: " + members + " members! :tada:");
+			}
 			welcomeMessages.forEach(function(msg, i) {
 				setTimeout(function(){
 					robot.send({room: res.message.user.name}, msg);
