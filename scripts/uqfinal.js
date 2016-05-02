6513b83d094a3a8b0d29f0456780cd28a08434cc
lewisjb <lbobbermen@bigpond.com>
2016-05-02 16:44:22 +1000

    UQFinal: Added evaluation, credits and more.  - MathJS evaluation  - If 0 < score < 1 then it is multiplied by 100 (ie 0.5 -> 50%) and    gives a note about it  - Added a "Powered by http://uqfinal.com" message

--- scripts/uqfinal.js
+++ scripts/uqfinal.js
@@ -5,6 +5,8 @@
 //   !`uqfinal|wdinotf|final` _<course>_ _<score1>_ _<score2>_ ... - Returns minimum mark needed on final exam
 //
 
+var math = require("mathjs");
+
 module.exports = function (robot) {
 	robot.respond(/!?(uqfinal|wdinotf|final) ([a-z]{4}[0-9]{4}[a-z]?) (.+)+/i, function (res) {
 		//res.match[2] is the course code
@@ -21,22 +23,26 @@ module.exports = function (robot) {
 						var num_of_assessment = course.assessment.length;
 						var scores = res.match[3].split(" ");
 						if(scores.length != num_of_assessment - 1) {
-							res.send("Invalid number of arguments. Course has " + num_of_assessment + " pieces of assessment, you gave " + scores.length + ". Keep in mind you need to leave the last piece of assessment out.");
+							res.send("Invalid number of arguments. Course has " + num_of_assessment + " pieces of assessment, you gave " + scores.length + ".\nKeep in mind you need to leave the last piece of assessment out.");
 						}else {
 							var total = 0;
 							var invalid = false;
 							for(var i = 0; i < num_of_assessment - 1; i++) {
-								if(isNaN(Number(scores[i]))) {
+								var evaluated = math.eval(scores[i]);
+								if(isNaN(evaluated)) {
 									res.send("Invalid score: '" + scores[i] + "'.");
 									invalid = true;
 									break;
+								}else if(evaluated > 0 && evaluated < 1) {
+									res.send("_Note: Treating '" + scores[i] + "' (" + evaluated + ") as: " + evaluated*100 + "%_");
+									evaluated *= 100;
 								}
-								total += (scores[i]/100) * course.assessment[i].weight;
+								total += (evaluated/100) * course.assessment[i].weight;
 							}
 							if(!invalid) {
 								var needed = 50 - total;
 								var result = Math.ceil(needed/course.assessment[num_of_assessment - 1].weight * 100);
-								res.send("You need to achieve at least " + result + "% on the final exam. _Disclaimer: this does not take hurdles into account_");
+								res.send("You need to achieve at least " + result + "% on the final exam.\n_Disclaimer: this does not take hurdles into account_\n_Powered by http://uqfinal.com_");
 							}
 						}
 					}
