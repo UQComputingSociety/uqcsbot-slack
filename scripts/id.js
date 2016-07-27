89a4431f0f2224a35bcb125a3ebd4522143e6f80
lewisjb <lbobbermen@bigpond.com>
2016-07-27 21:22:38 +1000

    Fixed deleted account filtering & added !whoami

--- scripts/id.js
+++ scripts/id.js
@@ -9,4 +9,8 @@ module.exports = function (robot) {
     robot.respond(/!?id/i, function(res) {
         res.send(res.envelope.user.id);
     });
+    
+    robot.respond(/!?whoami/i, function(res) {
+        res.send("```" + JSON.stringify(robot.brain.userForId(res.envelope.user.id), null, 4) + "```");
+    });
 };
