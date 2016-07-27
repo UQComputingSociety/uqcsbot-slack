6be378027e55883998ce5a11008e430ca2dc8c25
lewisjb <lbobbermen@bigpond.com>
2016-07-27 14:52:22 +1000

    Converted welcome to HTTP Cat

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -13,39 +13,6 @@ var welcomeMessages = [
 	"and again, welcome :)"
 ];
 
-// NUMBER OF PEOPLE: MESSAGE
-var unique_messages = {
-	400: "Bad Request",
-	401: "Unauthorized",
-	402: "Payment Required",
-	403: "Forbidden",
-	404: "Not Found",
-	405: "Method Not Allowed",
-	406: "Not Acceptable",
-	407: "Proxy Authentication Required",
-	408: "Request Timeout",
-	409: "Conflict",
-	410: "Gone",
-	411: "Length Required",
-	412: "Precondition Failed",
-	413: "Payload Too Large",
-	414: "URI Too Long",
-	415: "Unsupported Media Type",
-	416: "Range Not Satisfiable",
-	417: "Expectation Failed",
-	418: "I'm a teapot",
-	420: "Smoke weed everyday",
-	421: "Misdirected Request",
-	422: "Unprocessable Entity",
-	423: "Locked",
-	424: "Failed Dependency",
-	426: "Upgrade Required",
-	428: "Precondition Required",
-	429: "Too Many Requests",
-	431: "Request Header Fields Too Large",
-	451: "Unavailable For Legal Reasons",
-	666: "HAIL SATAN"
-};
 
 //The time between each individual welcome message send
 var messageTime = 2500;
@@ -58,13 +25,13 @@ module.exports = function(robot){
 			var members = active.length;
 
 			res.send("Welcome, " + res.message.user.name + "!");
-			var unique = unique_messages[members];
-			if(unique !== undefined) {
-				setTimeout(function() {
-					res.send(members + ": " + unique);
-				}, messageTime);
-			}
 
+			var url = "https://http.cat/" + members;
+			robot.http(url).get() (function (err, resp, body) {
+				if(!err) {
+					res.send(url);
+				}
+			});
 			if(members % 50 == 0) {
 				setTimeout(function() {
 					res.send(":tada: " + members + " members! :tada:");
