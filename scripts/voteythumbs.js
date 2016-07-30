0645e9345622920fb91e68d5210cd4aee1c30003
lewisjb <lbobbermen@bigpond.com>
2016-07-30 19:43:49 +1000

    Fixed voteythumbs

--- scripts/voteythumbs.js
+++ scripts/voteythumbs.js
@@ -20,20 +20,25 @@ module.exports = function (robot) {
 			if(votes === null) { return; }
 		}
 		//Get channel count
-		var channel_name = id.split("-")[0];
-		var channel = robot.adapter.client.getChannelGroupOrDMByID(channel_name);
-		var active = channel.members.filter(function(user) {
-			var slack = robot.brain.userForId(user).slack;
-			return slack.deleted === false && slack.is_bot === false;
-		});
-		var to_pass = active.length / 2;
+		var channel_id = id.split("-")[0];
+		var channel = robot.adapter.client.getChannelGroupOrDMByID(channel_id);
+		if(channel.members === undefined) {
+			//DM
+			var active = 1;
+		}else {
+			var active = channel.members.filter(function(user) {
+				var slack = robot.brain.userForId(user).slack;
+				return slack.deleted === false && slack.is_bot === false;
+			}).length;
+		}
+		var to_pass = active / 2;
 		if(votes.votes[id].count > to_pass) {
 			//Passed
-			robot.messageRoom(channel_name, "Vote: " + vote.votes[id].text + ". Passed\r\n");
+			robot.messageRoom(channel_id, "Vote: " + votes.votes[id].text + ". Passed\r\n");
 			votes.votes[id] = undefined;
 		}else if(votes.votes[id].count < -to_pass) {
 			//Failed
-			robot.messageRoom(channel_name, "Vote: " + vote.votes[id].text + ". Failed\r\n");
+			robot.messageRoom(channel_id, "Vote: " + votes.votes[id].text + ". Failed\r\n");
 			votes.votes[id] = undefined;
 		}
 	};
@@ -48,16 +53,22 @@ module.exports = function (robot) {
 		var item = res.message.rawMessage;
 		votes.votes[get_id(item.channel, item.ts, item.user)] = {count: 0, text: res.match[1]};
 
-		var add_reaction = function(item, emoji) {
+		var add_reaction = function(item, emoji, callback) {
 			robot.http("https://slack.com/api/reactions.add?token="
 					+ process.env.HUBOT_SLACK_TOKEN
 					+ "&name=" + emoji
 					+ "&channel=" + item.channel
 					+ "&timestamp=" + item.ts).get() (
-						function(err, resp, body) {});
+						function(err, resp, body) {
+							if(resp.error) {
+								console.log("Adding reaction failed: " + resp.error);
+							}
+							if(callback) { callback(); }
+						});
 		};
-		add_reaction(item, "thumbsup");
-		add_reaction(item, "thumbsdown");
+		add_reaction(item, "thumbsup", function() {
+			add_reaction(item, "thumbsdown");
+		});
 	};
 	robot.respond(/!?voteythumbs:? (.+)/, function (res) {
 		voteythumbs_message(res);
@@ -69,10 +80,11 @@ module.exports = function (robot) {
 		var votes = robot.brain.get("voteythumbs");
 		if(votes === null) {
 			reset_brain();
+			votes = robot.brain.get("voteythumbs");
 			if(votes === null) { return; }
 		}
-
 		if(message.type === "reaction_added") {
+			if(robot.brain.userForId(message.user).slack.is_bot) { return; }
 			var item = message.item;
 			var emoji = 0;
 			var id = get_id(item.channel, item.ts, message.item_user);
@@ -84,6 +96,7 @@ module.exports = function (robot) {
 			votes.votes[id].count += emoji;
 			check_if_passed(id);
 		}else if(message.type === "reaction_removed") {
+			if(robot.brain.userForId(message.user).slack.is_bot) { return; }
 			var item = message.item;
 			var emoji = 0;
 			var id = get_id(item.channel, item.ts, message.item_user);
