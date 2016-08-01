dcd044ba5552a9507b9f3cd305d0cdf60423d09e
lewisjb <lbobbermen@bigpond.com>
2016-08-01 15:06:17 +1000

    Fixed voteythumbs count & new message
     - Voteythumbs previously required the number of up votes - down votes
       to be greater than half the channel. Now it just requires >50% up or
       >50% down

--- scripts/voteythumbs.js
+++ scripts/voteythumbs.js
@@ -32,13 +32,13 @@ module.exports = function (robot) {
 			}).length;
 		}
 		var to_pass = active / 2;
-		if(votes.votes[id].count > to_pass) {
+		if(votes.votes[id].ups > to_pass) {
 			//Passed
-			robot.messageRoom(channel_id, "Vote: " + votes.votes[id].text + ". Passed\r\n");
+			robot.messageRoom(channel_id, ":white_check_mark: Voteythumbs '" + votes.votes[id].text + "'. Passed :white_check_mark:\r\n");
 			votes.votes[id] = undefined;
-		}else if(votes.votes[id].count < -to_pass) {
+		}else if(votes.votes[id].downs > to_pass) {
 			//Failed
-			robot.messageRoom(channel_id, "Vote: " + votes.votes[id].text + ". Failed\r\n");
+			robot.messageRoom(channel_id, ":x: Voteythumbs  '" + votes.votes[id].text + "'. Failed :x:\r\n");
 			votes.votes[id] = undefined;
 		}
 	};
@@ -51,7 +51,7 @@ module.exports = function (robot) {
 		}
 
 		var item = res.message.rawMessage;
-		votes.votes[get_id(item.channel, item.ts, item.user)] = {count: 0, text: res.match[1]};
+		votes.votes[get_id(item.channel, item.ts, item.user)] = {ups: 0, downs: 0, text: res.match[1]};
 
 		var add_reaction = function(item, emoji, callback) {
 			robot.http("https://slack.com/api/reactions.add?token="
@@ -86,26 +86,28 @@ module.exports = function (robot) {
 		if(message.type === "reaction_added") {
 			if(robot.brain.userForId(message.user).slack.is_bot) { return; }
 			var item = message.item;
-			var emoji = 0;
 			var id = get_id(item.channel, item.ts, message.item_user);
 			if(votes.votes[id] === undefined) { return; }
 
-			if(message.reaction === "+1") { emoji = 1; }
-			if(message.reaction === "-1") { emoji = -1; }
+			if(message.reaction === "+1") {
+				votes.votes[id].ups += 1;
+			}else if(message.reaction === "-1") {
+				votes.votes[id].downs += 1;
+			}
 
-			votes.votes[id].count += emoji;
 			check_if_passed(id);
 		}else if(message.type === "reaction_removed") {
 			if(robot.brain.userForId(message.user).slack.is_bot) { return; }
 			var item = message.item;
-			var emoji = 0;
 			var id = get_id(item.channel, item.ts, message.item_user);
 			if(votes.votes[id] === undefined) { return; }
 
-			if(message.reaction === "+1") { emoji = 1; }
-			if(message.reaction === "-1") { emoji = -1; }
+			if(message.reaction === "+1") {
+				votes.votes[id].ups -= 1;
+			}else if(message.reaction === "-1") {
+				votes.votes[id].downs -= 1;
+			}
 
-			votes.votes[id].count -= emoji;
 			check_if_passed(id);
 		}
 	});
