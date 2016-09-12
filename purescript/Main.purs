a0e112d9f7bca278f1f0c4fb871cdd5b659e8079
Tom Manderson <tom.manderson@polymathian.com>
2016-09-12 17:22:54 +1000

    Add explicit types to Main.purs

--- purescript/Main.purs
+++ purescript/Main.purs
@@ -1,11 +1,16 @@
 module Main (setup) where
 
 import Prelude
-import Control.Monad.Eff.Console (log)
+import Hubot (Robot)
+import Control.Monad.Eff (Eff)
 import Data.Foldable (traverse_)
 
 
+type Script eff = Robot -> Eff (eff) Unit
+
+scripts :: forall eff. Array (Script (eff))
 scripts = [
 ]
 
+setup :: forall eff. Script eff
 setup r = traverse_ (\x -> x r) scripts
