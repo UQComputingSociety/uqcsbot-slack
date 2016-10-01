731fe8605addd73c5fe7d289832dfb0e5390b91a
Tom Manderson <me@trm.io>
2016-10-01 23:11:50 +1000

    In-place edit of welcome.js

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -6,9 +6,8 @@ var welcomeMessages = [
     "Hey there! Welcome to the UQCS slack!",
     "This is the first time I've seen you, so you're probably new here",
     "I'm UQCSbot, your friendly (open source) robot helper",
-    "Introduce yourself on #intros, join some channels",
-    "We've got a bunch of generic channels (e.g. #banter, #games, #projects) along with some subject-specific ones",
-    "Your friendly admins are @trm, @svict4, @gricey, @rachcatch, @ainsleynand, @dmarj97",
+    "We've got a bunch of generic channels (e.g. #banter, #games, #projects) along with many subject-specific ones",
+    "Your friendly admins are @trm, @svict4, @gricey, @csa, @ainz, @dmarj97",
     "Type \"help\" here, or \"uqcsbot help\" anywhere else to find out what I can do.",
     "and again, welcome :)"
 ];
@@ -17,9 +16,7 @@ var messageTime = 2500;
 module.exports = function (robot) {
     robot.enter(function (res) {
         if (res.message.room == "general") {
-            var general = robot.adapter.client.getChannelGroupOrDMByName("general");
-            var active = general.members.filter(function (user) { return robot.brain.userForId(user).slack.deleted === false; }); // Filter out deleted accounts
-            var members = active.length;
+            var members = 99999999999;
             res.send("Welcome, " + res.message.user.name + "!");
             var url = "https://http.cat/" + members;
             robot.http(url).get()(function (err, resp, body) {
