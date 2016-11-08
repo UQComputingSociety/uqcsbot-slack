7a304a27eb2622212fa329beef5fe55d9112c79c
Jack Kerr <jack775544@gmail.com>
2016-11-08 13:36:09 +1000

    Added in id search for !xkcd
    Are you happy @lewisjb

--- scripts/xkcd.js
+++ scripts/xkcd.js
@@ -6,25 +6,56 @@
 //   Searches for an xkcd comic that is appropriate for that phrase
 module.exports = function (robot) {
   robot.respond(/!?xkcd ?(.+)?/i, function (res) {
+
+    /**
+     * This function is the reason why we can't have nice things
+     */
+    var isNumber = function (n) {
+      return !isNaN(n);
+    }
+
+    /**
+     * Search for xkcd comic by id
+     */
+    var idSearch = function (id) {
+      var url = "https://www.xkcd.com/" + id + "/"
+      robot.http(url).get()(function (err, resp, body) {
+        if (resp.statusCode != 200) {
+          res.send("Search on id `" + id + "` gives: \n That id isn't an id at all!");
+          return;
+        }
+        res.send("Search on id `" + id + "` gives: \n" + url);
+      });
+    }
+
+    /**
+     * Search for xkcd comic on text
+     */
+    var textSearch = function (comic) {
+      // Build the search URL
+      var url = "https://relevantxkcd.appspot.com/process?action=xkcd&query=" + comic;
+      robot.http(url).get()(function (err, resp, body) {
+        // We get sent back a newline delimited list so split on that
+        var r = body.split(/\n/);
+        // We get sent back a the query execution time, a 0 (for success I assume?)
+        // and n >= 1 result lines
+        r = r[2].split(" ");
+        // Each result line has the format `comiccode wikiurl`, and we want the code
+        res.send("Search for `" + comic + "` gives: \n" +
+          "https://www.xkcd.com/" + r[0] + "/");
+        return
+      });
+    }
+
     // Get the query string
     var comic = res.match[1]
     if (!comic) {
       res.send("You actually need to search for something");
       return;
+    } else if (isNumber(comic)) {
+      idSearch(comic);
+    } else {
+      textSearch(comic);
     }
-
-    // Build the search URL
-    var url = "https://relevantxkcd.appspot.com/process?action=xkcd&query=" + comic;
-    robot.http(url).get()(function (err, resp, body) {
-      // We get sent back a newline delimited list so split on that
-      var r = body.split(/\n/);
-      // We get sent back a the query execution time, a 0 (for success I assume?)
-      // and n >= 1 result lines
-      r = r[2].split(" ");
-      // Each result line has the format `comiccode wikiurl`, and we want the code
-      res.send("Search for `" + comic + "` gives: \n" + 
-        "https://www.xkcd.com/" + r[0] + "/");
-      return
-    });
   });
 };
