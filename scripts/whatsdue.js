64bfd91c6b80257c3345b0815c26655ac9db804b
mitchmcdee <mitchmcdee@gmail.com>
2017-03-13 22:09:29 +1000

    Added whatsdue command
    Prints a list of assessment due given a list of course codes. Initial commit, functionality may change :)

---
+++ scripts/whatsdue.js
@@ -0,0 +1,131 @@
+// Description
+//   Prints all assessment for a given array of courses.
+//
+// Commands:
+//   !`whatsdue`` _<course1>_ _<course2>_ ... - Prints all assessment for a
+//												given array of courses.
+
+var cheerio = require("cheerio");
+
+module.exports = function (robot) {
+
+	// The course and assessment urls to get profile and assessment info.
+	var courseUrl = 'https://www.uq.edu.au/study/course.html?course_code=';
+	var assessmentUrl = 'https://www.courses.uq.edu.au/student_section_report' +
+						'.php?report=assessment&profileIds=';
+
+	/**
+	 * Gets the profile id for a course given its course code.
+	 *
+	 * @param {string} course code (eg. CSSE2310)
+	 * @return {!Promise} A promise that resolves with the profile id
+	 *					  or an error.
+	 */
+    function getProfileId(course) {
+    	return new Promise((resolve, reject) => {
+	        robot.http(courseUrl + course).get() ((err, resp, body) => {
+	    		if (err) {
+	    			reject("There was an error getting the course profile.");
+	    			return;
+	    		}
+
+	    		// Look for first instance of `/profileId=`. Will always point
+	    		// to the latest profile id for the course.
+	            var profileID = String(body.match(/profileId=\d*/));
+	            if (profileID !== "null") {
+	                resolve(profileID.replace('profileId=',''));
+	            } else {
+	            	reject(course + " is not a valid course code.");
+	            }
+	    	});
+        });
+    }
+
+	/**
+	 * Parses the assessment data provided in the assessment url and returns
+	 * an array of assessments.
+	 *
+	 * @param {string} url linking to the assessment data
+	 * @return {!Promise} A promise that resolves with an array of assessments
+	 *					  or an error.
+	 */
+    function parseAssessmentData(url) {
+    	return new Promise(resolve => {
+    		robot.http(url).get() ((err, resp, body) => {
+	    		if (err) {
+	    			reject("There was an error getting the assessment.");
+	    			return;
+	    		}
+
+				var $ = cheerio.load(body);
+				var assessment = [];
+
+				// Look for the tblborder class, which is the assessment data
+				// table, then loop over its children starting at index 1 to
+				// skip over the column headers (subject, task, due date and
+				// weighting).
+	    		$('.tblborder').children().slice(1).each((index, element) => {
+	    			var columns = $(element).children();
+
+	    			var subject = $(columns[0]).find('div')
+	    						  .text().trim().slice(0, 8);
+	    			var task = $(columns[1]).find('div')
+	    			           .html().trim().replace('<br>', ' - ');
+	    			var dueDate = $(columns[2]).find('div')
+	    					   	  .text().trim();
+	    			var weighting = $(columns[3]).find('div')
+	    							.text().trim();
+
+	    			if (!subject || !task || !dueDate || !weighting) {
+	    				reject("There was an error parsing the assessment.");
+	    				return;
+	    			}
+
+	    			assessment.push([subject, task, dueDate, weighting]);
+	    		});
+
+	    		resolve(assessment);
+    		});
+    	});
+    }
+
+	/**
+	 * Prints the given assessment array with proper formatting.
+	 *
+	 * @param {Response} instance of robot.Response
+	 * @param {Object} array of assessments
+	 */
+    function printAssessment(res, assessmentArray) {
+    	assessmentArray.forEach(assessment => {
+			res.send(assessment[0] + " requires " +
+					 assessment[1] + " to be done by " +
+					 assessment[2] + " and is worth " +
+					 assessment[3]);
+		});
+    }
+
+	/**
+	 * Robot responds to a message containing `!whatsdue`.
+	 * Requires at least one course.
+	 */
+    robot.respond(/!?whatsdue ((?:[a-z]{4}[0-9]{4} ?)+)/i, function (res) {
+    	// The regex groups the courses together as a single string, we can
+    	// obtain an array of them by splitting at the whitespaces.
+    	var courses = res.match[1].split(" ");
+    	
+    	// Create a Promise for each course.
+    	var profileResponses = [];
+    	for (var i = 0; i < courses.length; i++) {
+    		profileResponses.push(getProfileId(courses[i]));
+    	}
+
+    	// Resolve all the Promises to obtain an array of profile ids. Join
+    	// them together to create the necessary assessment url to parse and
+    	// display back to the user. Print any errors that occured.
+    	Promise.all(profileResponses)
+    		.then(profiles => assessmentUrl + profiles.join())
+    		.then(url => parseAssessmentData(url))
+    		.then(assessment => printAssessment(res, assessment))
+    		.catch(error => { res.send(error); });
+    });
+};
