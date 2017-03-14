349e6fcfbdba4fa225aa851cc9763c83e665d633
mitchmcdee <mitchmcdee@gmail.com>
2017-03-15 02:44:54 +1000

    Updated ecp script. Fixed ecp for course room. Cleaned up some things and added error checking.

--- scripts/ecp.js
+++ scripts/ecp.js
@@ -2,32 +2,34 @@
 //   Finds links for electronic course profiles.
 //
 // Commands:
-//   !`ecp /[a-zA-Z]{4}\d{4}/` - Finds the link to the ECP for the given course profile
+//   !`ecp /[a-z]{4}[0-9]{4}/` - Finds the link to the ECP for the given course profile
 //   !`ecp ENGG2800` - Shows the electronic course profile of ENGG2800
 //   !`ecp ENGG2801` - Prints an error message
 module.exports = function (robot) {
 
-    function printCourse(res, course) {
+    function getEcp(course, onComplete) {
         var URL = "https://www.uq.edu.au/study/course.html?course_code=" + course;
+
         robot.http(URL).get() (function(err, resp, body) {
+            if (err) {
+                reject('There was an error getting the course profile.');
+                return;
+            }
+
             var profileID = String(body.match(/profileId=\d*/));
             if (profileID === "null") {
-                res.send(["Course \"", course, "\" does not exist."].join(""));
-            } else {
-                var link = "www.courses.uq.edu.au/student_section_loader.php?section=1&";
-                res.send(["The ECP of \"", course, "\" is located at ", link.concat(profileID)].join(""));
+                onComplete("Course '" + course + "' does not exist.");
+                return;
             }
+
+            var link = "www.courses.uq.edu.au/student_section_loader.php?section=1&" + profileID;
+            onComplete("The ECP of '" + course + "' is located at " + link);
         });
     }
-    // respond to !`ecp`
-    robot.respond(/!?ecp$/, function (res) {
-        var course = String(res.room);
-        printCourse(res, course);
-    });
 
-    // respond to !`ecp ENGG2800`
-    robot.respond(/!?ecp ([a-zA-Z]{4}\d*)$/, function (res) {
-        var course = res.match[1];
-        printCourse(res, course);
+    // respond to !`ecp` or !`ecp ENGG2800`
+    robot.respond(/!?ecp ?([a-z]{4}[0-9]{4})?$/i, function (res) {
+        var course = res.match[1] || res.message.room;
+        getEcp(course, ecp => { res.send(ecp) });
     });
 };
