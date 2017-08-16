d5be4603ce4e4d93ed6e93a899edb840c8b7e24c
mitchmcdee <mitchmcdee@gmail.com>
2017-08-16 19:43:32 +1000

    Adding calendar to whatdue

--- scripts/whatsdue.js
+++ scripts/whatsdue.js
@@ -2,24 +2,23 @@
 //   Prints all assessment for a given array of courses.
 //
 // Commands:
-//   !`whatsdue`` _<course1>_ _<course2>_ ... - Prints all assessment for a
-//                                              given array of courses.
+//   !`whatsdue`` _<course1>_ _<course2>_ ... - Prints all assessment for a given array of courses.
 
 var cheerio = require('cheerio');
+var ical = require('ical.js');
+var Readable = require('stream').Readable;
 
 module.exports = function (robot) {
 
     // The course and assessment urls to get profile and assessment info.
     var courseUrl = 'https://www.uq.edu.au/study/course.html?course_code=';
-    var assessmentUrl = 'https://www.courses.uq.edu.au/student_section_report' +
-                        '.php?report=assessment&profileIds=';
+    var assessmentUrl = 'https://www.courses.uq.edu.au/student_section_report.php?report=assessment&profileIds=';
 
     /**
      * Gets the profile id for a course given its course code.
      *
      * @param {string} course code (eg. CSSE2310)
-     * @return {!Promise} A promise that resolves with the profile id
-     *                    or an error.
+     * @return {!Promise} A promise that resolves with the profile id or an error.
      */
     function getProfileId(course) {
         return new Promise((resolve, reject) => {
@@ -29,15 +28,13 @@ module.exports = function (robot) {
                     return;
                 }
 
-                // Look for first instance of `/profileId=`. Will always point
-                // to the latest profile id for the course. If there is no
-                // profileId, check to ensure the course code is valid and return
-                // the relevant error message.
+                // Look for first instance of `/profileId=`. Will always point to the latest profile id for the course.
+                // If there is no profileId, check to ensure the course code is valid and return the relevant error
+                // message.
                 var profileID = String(body.match(/profileId=\d*/));
                 if (profileID !== 'null') {
                     resolve(profileID.replace('profileId=',''));
-                } else if (body.match(/is not a valid course code/) || 
-                           body.match(/Unable to find course code/)) {
+                } else if (body.match(/is not a valid course code/) || body.match(/Unable to find course code/)) {
                     reject(course + ' is not a valid course code.');  
                 } else {
                     reject(course + ' has no available course profiles.');  
@@ -47,14 +44,12 @@ module.exports = function (robot) {
     }
 
     /**
-     * Parses the assessment data provided in the assessment url and returns
-     * an array of assessments.
+     * Parses the assessment data provided in the assessment url and returns an array of assessments.
      *
      * @param {string} url linking to the assessment data
-     * @return {!Promise} A promise that resolves with an array of assessments
-     *                    or an error.
+     * @return {!Promise} A promise that resolves with an array of assessments or an error.
      */
-    function parseAssessmentData(url) {
+    function getAssessment(url) {
         return new Promise(resolve => {
             robot.http(url).get() ((err, resp, body) => {
                 if (err) {
@@ -63,43 +58,87 @@ module.exports = function (robot) {
                 }
 
                 var $ = cheerio.load(body);
-                var assessment = '_*WARNING:* Assessment information may ' +
-                                 'vary/change/be entirely different! Use ' +
-                                 'at your own discretion_\r\n>>>';
+                assessment = [];
 
-                // Look for the tblborder class, which is the assessment data
-                // table, then loop over its children starting at index 1 to
-                // skip over the column headers (subject, task, due date and
-                // weighting).
+                // Look for the tblborder class, which is the assessment data table, then loop over its children
+                // starting at index 1 to skip over the column headers (subject, task, due date and weighting).
+                //
                 // TODO(mitch): make this less ugly and bleh.
-                // Note: Formatting of assessment data is inconsistent and may
-                //       look ugly. Soz.
+                //
+                // Note: Scraped assessment data is inconsistently formatted and may my assumptions below may make it
+                // look ugly. Soz, tell UQ to standardise their shiet.
                 $('.tblborder').children().slice(1).each((index, element) => {
                     var columns = $(element).children();
 
-                    var subject = $(columns[0]).find('div')
-                                  .text().trim().slice(0, 8);
-                    var task = $(columns[1]).find('div')
-                               .html().trim().replace('<br>', ' - ');
-                    var dueDate = $(columns[2]).find('div')
-                                  .text().trim();
-                    var weighting = $(columns[3]).find('div')
-                                    .text().trim();
+                    var subject = $(columns[0]).find('div').text().trim().slice(0, 8);
+                    var task = $(columns[1]).find('div').html().trim().replace('<br>', ' - ');
+                    var dueDate = $(columns[2]).find('div').text().trim();
+                    var weighting = $(columns[3]).find('div').text().trim();
 
                     if (!subject || !task || !dueDate || !weighting) {
                         reject('There was an error parsing the assessment.');
                         return;
                     }
 
-                    assessment += '*' + subject + '*: ' +
-                                  '`' + task + '` ' +
-                                  '_(' + dueDate + ')_ ' +
-                                  '*' + weighting + '*\r\n';
+                    assessment.push([subject, weighting, task, dueDate]);
                 });
 
                 resolve(assessment);
             });
         });
+    }
+
+    /**
+     * 
+     */
+    function getFormattedAssessment(assessment) {
+        var formattedAssessment = '_*WARNING:* Assessment information may vary/change/be entirely different! Use at ' +
+                                  'your own discretion_\r\n>>>';
+
+        // Loop over each assessment and concatentate the subject, task, due date and weighting in nice formatting.
+        assessment.map(function(a) {
+            formattedAssessment += '*' + a[0] + '*: `' + a[1] + '` _' + a[2] + '_ *(' + a[3] + ')*\r\n';
+        })
+
+        return formattedAssessment;
+    }
+
+    /**
+     * Generate iCal calendar containing the assessment pieces and return a link to it.
+     */
+    function getCalendar(assessment, userChannel) {
+            //
+            var cal = new ical.Component(['vcalendar', [], []]);
+            cal.updatePropertyWithValue('prodid', '-//uqcsbot whatsdue assessment generation');
+            cal.updatePropertyWithValue('version', '2.0');
+
+            //
+            assessment.map(function(a) {
+                var vevent = new ical.Component('vevent');
+                vevent.updatePropertyWithValue('dtstamp', ICAL.Time.now());
+
+                var event = new ical.Event(vevent);
+                event.summary = a[0] + ' ' + a[1] + ' ' + a[2] + ' ' + a[3];
+                event.uid = a[0];
+                event.startDate = new ical.Time.now();
+
+                cal.addSubcomponent(vevent);
+            });
+
+            //
+            return robot.adapter.client.web.files.upload('assessmentCalendar.ics', {
+                channels: userChannel,
+                title: 'Importable iCalendar containing your assessment',
+                initial_comment: 'Click the menu next to me and select "Copy Link" to import me as an URL',
+                file: {
+                    value: cal.toString(),
+                    options: {
+                        filename: 'assessmentCalendar.ics',
+                        contentType: 'text/calendar',
+                        knownLength: cal.toString().length
+                    }
+                }
+            });
     }
 
     /**
@@ -109,8 +148,7 @@ module.exports = function (robot) {
         var channel = null;
         // Get the channel name (and treat it as a course code!).
         if (robot.adapter.client && robot.adapter.client.rtm) {
-            channel = robot.adapter.client.rtm.dataStore
-                      .getChannelById(res.message.room);
+            channel = robot.adapter.client.rtm.dataStore.getChannelById(res.message.room);
         }
 
         // Check there is either a valid channel or a valid input.
@@ -119,8 +157,7 @@ module.exports = function (robot) {
             return;
         }
 
-        // If the user has provided a course list, use that; else, use the
-        // current channel as the course code.
+        // If the user has provided a course list, use that; else, use the current channel as the course code.
         var courses = (res.match[1]) ? res.match[1].split(' ') : [channel.name];
 
         // Create a Promise for each course.
@@ -129,13 +166,16 @@ module.exports = function (robot) {
             profileResponses.push(getProfileId(courses[i].toUpperCase()));
         }
 
-        // Resolve all the Promises to obtain an array of profile ids. Join
-        // them together to create the necessary assessment url to parse and
-        // display back to the user. Print any errors that occured.
+        // Resolve all the Promises to obtain an array of profile ids. Join them together to create the necessary
+        // assessment url to parse and display back to the user. Print any errors that occured.
         Promise.all(profileResponses)
             .then(profiles => assessmentUrl + profiles.join())
-            .then(url => parseAssessmentData(url))
-            .then(assessment => res.send(assessment))
+            .then(url => getAssessment(url))
+            .then(assessment => {
+                res.send(getFormattedAssessment(assessment));
+                return getCalendar(assessment, res.message.user.id);
+            })
+            .then(response => console.log(response))
             .catch(error => res.send(error));
     });
 };
