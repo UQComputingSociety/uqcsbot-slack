8e55d1aa7f16c715f5644ee3ead0408f08151518
Mitchell Grice <mitch@gricey.net>
2017-08-20 16:14:23 +1000

    Add a bom radar

---
+++ scripts/radar.js
@@ -0,0 +1,11 @@
+// Description
+//   Gets the Beureau of Meteorology's latest radar image
+// Commands
+//   !`radar` - Gets Brisbane's radar
+//
+
+module.exports = function (robot) {
+	robot.respond(/!?radar/i, function (res) {
+    res.send("https://bom.lambda.tools/?location=brisbane&_cache=" + Date.now());
+  });
+};
