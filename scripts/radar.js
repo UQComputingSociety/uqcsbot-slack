d9214dcb9ee87c5a337b1241a96c352a384ebe77
Mitchell Grice <mitch@gricey.net>
2017-08-21 12:34:32 +1000

    Spelling

--- scripts/radar.js
+++ scripts/radar.js
@@ -1,11 +1,11 @@
 // Description
-//   Gets the Beureau of Meteorology's latest radar image
+//   Gets the Bureau of Meteorology's latest radar image
 // Commands
 //   !`radar` - Gets Brisbane's radar
 //
 
 module.exports = function (robot) {
-	robot.respond(/!?radar/i, function (res) {
+  robot.respond(/!?radar/i, function (res) {
     res.send("https://bom.lambda.tools/?location=brisbane&_cache=" + Date.now());
   });
 };
