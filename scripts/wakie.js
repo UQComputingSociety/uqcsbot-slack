339c200bb4154c3a01c373054267d92ea3a429b9
mitchmcdee <mitchmcdee@gmail.com>
2017-10-23 12:59:51 +1100

    Patched wakie function to new conversations.members API

--- scripts/wakie.js
+++ scripts/wakie.js
@@ -1,21 +1,37 @@
 // Description
-//   Wakie Wakie - Pings a random person in #general and asks what they are up to
+//   Wakie Wakie - Pings two random people in #general and asks what they are up to
 
 var HubotCron = require('hubot-cronjob');
 
-module.exports = function(robot) {
-	var fn, pattern, timezone;
-	pattern = '0 17 * * *'; // Daily at 5:00PM
-	timezone = 'Australia/Brisbane';
+var API_LIMIT = 200;                 // Number of members to get at a time
+var PATTERN =  '59 11 * * *';        // Daily at 5:00PM
+var TIMEZONE = 'Australia/Brisbane'; // in B-town
 
-	fn = function() {
-	  console.log(robot.adapter.client.web.users.list);
-		robot.adapter.client.web.users.list({presence: 0}, function(ignored, result){
-            var members = result.members.filter(function (user) { return user.deleted == false; });
-            var victim = members[Math.floor(Math.random() * members.length)];
-			var victim2 = members[Math.floor(Math.random() * members.length)];
-            return robot.messageRoom("general", "Hey <@" + victim.id + ">! Tell us about something cool you are working on!\r\nHey <@" + victim2.id + ">! Tell us about something cool you are working on!");
+// Get a list of all the members in a room
+function getMembers(robot, room, members, cursor) {
+    // No more members to get, return the final list
+    if (cursor == "") {
+        return Promise.resolve(members);
+    }
+
+    // Concat this batch of members and request for next batch
+    return robot.adapter.client.web.conversations.members(room, {limit: API_LIMIT, cursor: cursor})
+        .then(res => getMembers(robot, room, members.concat(res.members), res.response_metadata.next_cursor));
+}
+
+module.exports = function(robot) {
+    // Wakie function that pings two random people in general
+	var wakieFunction = function() {
+        var announcements = robot.adapter.client.rtm.dataStore.getChannelByName("announcements").id;
+        getMembers(robot, announcements, []).then(members => {
+            var victim1 = members[Math.floor(Math.random() * members.length)];
+            var victim2 = members[Math.floor(Math.random() * members.length)];
+            robot.messageRoom("general", 
+                "Hey <@" + victim1 + ">! Tell us about something cool you are working on!\r\n" + 
+                "Hey <@" + victim2 + ">! Tell us about something cool you are working on!");
         });
 	};
-	return new HubotCron(pattern, timezone, fn);
+
+    // Export the cron job to run the wakie function at the specified time
+	return new HubotCron(PATTERN, TIMEZONE, wakieFunction);
 };
