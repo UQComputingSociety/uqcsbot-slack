4db73a3ffad8d1b77684e4654b535ab51ecaa4a6
mitchmcdee <mitchmcdee@gmail.com>
2017-10-23 17:53:07 +1100

    wavie v2.0

--- scripts/wavie.js
+++ scripts/wavie.js
@@ -1,23 +1,32 @@
 // Description
-//   Wave people as they enter and exit the room
+//   Wave at people as they joining/leaving a room
 
 module.exports = function (robot) {
-    // Wave at the latest message in the room
+    // Wave at the person joining/leaving the room
     function wave(room) {
         // Make sure we have access to all the clients we need
         if (!robot.adapter.client || !robot.adapter.client.web) {
             return;
         }
 
-        // Shorten web client so we can have nice concise lines :)
-        webClient = robot.adapter.client.web;
+        // Shorten web client so we can have nice concise lines ;)
+        var webClient = robot.adapter.client.web;
 
-        // Try channel as a public channel search first, then as a private channel
-        webClient.channels.info(room)
-            .then(res => webClient.reactions.add('wave', {channel: room, timestamp: res.channel.latest.ts}))
-            .catch(err => { if (err == 'channel_not_found') return webClient.groups.info(room); })
-            .then(res => webClient.reactions.add('wave', {channel: room, timestamp: res.channel.latest.ts}))
-            .catch(err => console.log);
+        // If room was not a channel (public) and not a group (private), exit
+        if (room[0] != 'C' && room[0] != 'G') {
+            return;
+        }
+
+        // Retrieve the room information and :wave: if the latest message is a person joining/leaving
+        var channelPromise = (room[0] == 'C') ? webClient.channels.info(room) : webClient.groups.info(room);
+        channelPromise.then(res => {
+            // If the latest message was not a person joining nor leaving, exit
+            if (res.channel.latest.subtype != 'channel_join' && res.channel.latest.subtype != 'channel_leave') {
+                return;
+            }
+
+            webClient.reactions.add('wave', {channel: room, timestamp: res.channel.latest.ts});
+        }).catch(err => console.log);
     }
 
     // Listen for when someone enters a room
