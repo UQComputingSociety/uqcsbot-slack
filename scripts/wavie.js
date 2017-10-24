c16b0fd59c3a8e9a434da89959266ec98fb7ff14
Mitchell McDermott <mitchmcdee@users.noreply.github.com>
2017-10-24 16:23:19 +1000

    small fix: now returns the reaction promise so we can catch any errors

--- scripts/wavie.js
+++ scripts/wavie.js
@@ -29,7 +29,7 @@ module.exports = function (robot) {
                 return;
             }
 
-            webClient.reactions.add('wave', {channel: room, timestamp: last.ts});
+            return webClient.reactions.add('wave', {channel: room, timestamp: last.ts});
         }).catch(err => console.log);
     }
 
