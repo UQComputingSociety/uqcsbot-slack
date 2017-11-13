ad73d389099d6f093313accac77bee7a37e334fb
mitchmcdee <mitchmcdee@gmail.com>
2017-11-14 01:29:32 +1000

    started making it work with multiple sub folders

--- scripts/attic.js
+++ scripts/attic.js
@@ -11,6 +11,32 @@ BASE_API_URL = 'https://www.googleapis.com/drive/v3/'
 BASE_ATTIC_FOLDER_ID = '0B6_D4T6LJ-uwZmFhMzIyNGYtNTM2OS00ZDJlLTg0NmYtY2IyNzA1MDZlNDIx'
 API_KEY = 'AIzaSyD6JDGpJMv-tPh-vOJB-MLr9_cqskdGmGA'
 
+function getAllFiles(robot, folderId) {
+    folderRequest = BASE_API_URL + `files?q='` + folderId + `' in parents&key=` + API_KEY;
+    return new Promise((resolve, reject) => {
+        robot.http(folderRequest).get()((err, resp, body) => {
+            if (err) {
+                reject('There was an error getting the course folder');
+                return;
+            }
+
+            var folderFiles = [];
+            JSON.parse(body).files.forEach(file => {
+                if (file.mimeType == 'application/vnd.google-apps.folder') {
+                    console.log(file.name);
+                    getAllFiles(robot, file.id).then(subFiles => {
+                        folderFiles.push(...subFiles);
+                    });
+                } else {
+                    folderFiles.push(`*${file.name}*: <${BASE_FILE_URL}${file.id}|Link>`);
+                }
+            });
+
+            resolve(folderFiles);
+        });
+    });
+}
+
 module.exports = function (robot) {
 	robot.respond(/!?attic ?(.*)/i, function (res) {
         var courseCode = res.match[1];
@@ -33,17 +59,7 @@ module.exports = function (robot) {
                 return;
             }
 
-            courseFolderRequest = BASE_API_URL + `files?q='` + courseFolder.id + `' in parents&key=` + API_KEY;
-            robot.http(courseFolderRequest).get() ((err, resp, body) => {
-                if (err) {
-                    console.log('There was an error getting the course folder');
-                    return;
-                }
-
-                courseFiles = JSON.parse(body).files.map(file => `*${file.name}*: <${BASE_FILE_URL}${file.id}|Link>`);
-                res.send('>>>' + courseFiles.join('\n'));
-            });
-
+            getAllFiles(robot, courseFolder.id).then(result => res.send('>>>' + result.join('\n')));
         });
   });
 };
