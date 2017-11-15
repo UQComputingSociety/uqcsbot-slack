445b33ab6e65d46c088d6102fdeb5786fa2d466e
mitchmcdee <mitchmcdee@gmail.com>
2017-11-15 13:54:03 +1000

    Okay recursive folders now working :)

--- scripts/attic.js
+++ scripts/attic.js
@@ -7,11 +7,23 @@
 BASE_FOLDER_URL = 'https://drive.google.com/drive/folders/'
 BASE_FILE_URL = 'https://drive.google.com/file/d/'
 BASE_API_URL = 'https://www.googleapis.com/drive/v3/'
-
-BASE_ATTIC_FOLDER_ID = '0B6_D4T6LJ-uwZmFhMzIyNGYtNTM2OS00ZDJlLTg0NmYtY2IyNzA1MDZlNDIx'
+BASE_ATTIC_FOLDER = '0B6_D4T6LJ-uwZmFhMzIyNGYtNTM2OS00ZDJlLTg0NmYtY2IyNzA1MDZlNDIx'
 API_KEY = 'AIzaSyD6JDGpJMv-tPh-vOJB-MLr9_cqskdGmGA'
+ROOM_FILE_LIMIT = 15; // Number of files allowed to be posted in room, else sent via direct message
 
 function getAllFiles(robot, folderId) {
+    return getFolderFiles(robot, folderId).then(files => {
+        return Promise.all(files.map(file => {
+            if (file.mimeType == 'application/vnd.google-apps.folder') {
+                return getAllFiles(robot, file.id);
+            } else {
+                return Promise.resolve(file);
+            }
+        }));
+    });
+}
+
+function getFolderFiles(robot, folderId) {
     folderRequest = BASE_API_URL + `files?q='` + folderId + `' in parents&key=` + API_KEY;
     return new Promise((resolve, reject) => {
         robot.http(folderRequest).get()((err, resp, body) => {
@@ -19,34 +31,26 @@ function getAllFiles(robot, folderId) {
                 reject('There was an error getting the course folder');
                 return;
             }
-
-            var folderFiles = [];
-            JSON.parse(body).files.forEach(file => {
-                if (file.mimeType == 'application/vnd.google-apps.folder') {
-                    console.log(file.name);
-                    getAllFiles(robot, file.id).then(subFiles => {
-                        folderFiles.push(...subFiles);
-                    });
-                } else {
-                    folderFiles.push(`*${file.name}*: <${BASE_FILE_URL}${file.id}|Link>`);
-                }
-            });
-
-            resolve(folderFiles);
+            resolve(JSON.parse(body).files);
         });
     });
+}
+
+function flattenArray(array) {
+    return array.reduce((a,b) => a.concat(Array.isArray(b) ? flattenArray(b) : b), []);
 }
 
 module.exports = function (robot) {
 	robot.respond(/!?attic ?(.*)/i, function (res) {
         var courseCode = res.match[1];
         if (!courseCode) {
-            res.send(BASE_FOLDER_URL + BASE_ATTIC_FOLDER_ID);
+            res.send(BASE_FOLDER_URL + BASE_ATTIC_FOLDER);
             return;
         }
         courseCode = courseCode.toUpperCase();
 
-        var baseFolderRequest = BASE_API_URL + `files?q='` + BASE_ATTIC_FOLDER_ID + `' in parents and mimeType = 'application/vnd.google-apps.folder'&key=` + API_KEY;
+        var baseFolderRequest = BASE_API_URL + `files?q='` + BASE_ATTIC_FOLDER +
+                                `' in parents and mimeType = 'application/vnd.google-apps.folder'&key=` + API_KEY;
         robot.http(baseFolderRequest).get() ((err, resp, body) => {
             if (err) {
                 res.send('There was an error getting the base folder');
@@ -59,7 +63,17 @@ module.exports = function (robot) {
                 return;
             }
 
-            getAllFiles(robot, courseFolder.id).then(result => res.send('>>>' + result.join('\n')));
+            getAllFiles(robot, courseFolder.id).then(files => {
+                var flatFiles = flattenArray(files);
+                var formattedFiles = flatFiles.map(file => `> *${file.name}*: <${BASE_FILE_URL}${file.id}|Link>`);
+                if (formattedFiles.length > ROOM_FILE_LIMIT) {
+                    user = res.message.user;
+                    res.send(`Too many files to display, sent results directly to ${user.name}`);
+                    robot.send({room: user.id}, formattedFiles.join('\n'));
+                } else {
+                    res.send(formattedFiles.join('\n'));
+                }
+            }).catch(err => console.log(err));
         });
   });
 };
