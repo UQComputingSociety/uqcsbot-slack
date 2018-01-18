3519983e18b9944061f3b117b23acb6f0bcac6f8
Cameron Aavik <cameronaavik@gmail.com>
2018-01-18 17:37:20 +1000

    Fresh start in python

--- purescript/Main.purs
+++
@@ -1,9 +0,0 @@
-module Main (setup) where
-import Prelude
-import Scripts (scripts, Script')
-import Data.Foldable (traverse_)
-import Control.Monad.Eff.Exception (message, catchException)
-import Control.Monad.Eff.Console (error)
-
-setup :: Script'
-setup r = traverse_ ((flip ($) r) >>> catchException (error <<< message)) scripts
