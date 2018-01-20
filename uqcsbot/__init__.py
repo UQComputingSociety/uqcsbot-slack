a1f54a9b8b6976eb8b761e791b1c766de2b38685
Tom Manderson <me@trm.io>
2018-01-20 21:45:02 +1000

    Project structure

---
+++ uqcsbot/__init__.py
@@ -0,0 +1,20 @@
+import os
+import importlib
+from .base import slack_events_adapter
+
+
+
+def main():
+	dir_path = os.path.dirname(__file__)
+	scripts_dir = os.path.join(dir_path, 'scripts')
+	for subfile in os.path.listdir(scripts_dir):
+		if not subfile.endswith('.py') or subfile == '__init__.py':
+			continue
+		module = f'uqcsbot.scripts.{subfile[:-3]}'
+		importlib.import_module(module)
+
+
+	slack_events_adapter.start(port=5000)
+
+if __name__ == "__main__":
+	main()
