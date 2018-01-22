0d784927e5f81275dc47dd82cf3cd0afc2bea4c5
Cameron Aavik <cameronaavik@gmail.com>
2018-01-22 19:38:33 +1000

    make the slack client and event adapter local only as opposed to globa then break all the other scripts :'(

--- uqcsbot/__init__.py
+++ uqcsbot/__init__.py
@@ -1,10 +1,33 @@
 import os
 import sys
 import importlib
-from uqcsbot.base import slack_events_adapter, bot, command_handler
-from uqcsbot.stub import ClientStub
+from slackclient import SlackClient
+from slackeventsapi import SlackEventAdapter
+from uqcsbot.command_handler import CommandHandler
+from uqcsbot.stub import ClientStub, EventAdapterStub
+from uqcsbot.api import Bot
+
+SLACK_VERIFICATION_TOKEN = os.environ.get("SLACK_VERIFICATION_TOKEN", "")
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "")
+
+command_handler: CommandHandler = None
+bot: Bot = None
+
 
 def main():
+    if '--dev' in sys.argv:
+        client = ClientStub()
+        event_adapter = EventAdapterStub()
+    else:
+        client = SlackClient(SLACK_BOT_TOKEN)
+        event_adapter = SlackEventAdapter(SLACK_VERIFICATION_TOKEN, "/uqcsbot/events")
+
+    # this doesn't work
+    global command_handler
+    global bot
+    command_handler = CommandHandler(client, event_adapter)
+    bot = Bot(client)
+
     dir_path = os.path.dirname(__file__)
     scripts_dir = os.path.join(dir_path, 'scripts')
     for sub_file in os.listdir(scripts_dir):
@@ -13,12 +36,7 @@ def main():
         module = f'uqcsbot.scripts.{sub_file[:-3]}'
         importlib.import_module(module)
 
-    if '--dev' in sys.argv:
-        stub = ClientStub(command_handler)
-        bot.client = stub
-        stub.adapter.prompt()
-    else:
-        slack_events_adapter.start(port=5000)
+    event_adapter.start(port=5000)
 
 if __name__ == "__main__":
     main()
--- uqcsbot/stub.py
+++ uqcsbot/stub.py
@@ -1,24 +1,21 @@
 from pyee import EventEmitter
-from uqcsbot.command_handler import CommandHandler
+
 
 class ClientStub(object):
-    def __init__(self, handler: CommandHandler):
-        self.adapter = EventAdapterStub()
-        self.adapter.on("message", handler.handle_command)
+    def api_call(self, method, **kwargs):
+        if method == "chat.postMessage":
+            print(kwargs['text'])
 
-    def api_call(self, call: str, channel: str, text: str):
-        if call == "chat.postMessage":
-            print(text)
 
 class EventAdapterStub(EventEmitter):
-    def prompt(self):
-        response = input("> ")
-        message = {
-            "event" : {
-                "text" : response,
-                "channel" : "general",
-                "subtype" : "user"
+    def start(self):
+        while True:
+            response = input("> ")
+            message = {
+                "event": {
+                    "text": response,
+                    "channel": "general",
+                    "subtype": "user"
+                }
             }
-        }
-        self.emit("message", message)
-        self.prompt()
+            self.emit("message", message)
