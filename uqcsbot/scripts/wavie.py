a586cc268e8a7f727ccad040b3ef37016cf06273
Tom Manderson <me@trm.io>
2018-02-12 17:50:01 +1000

    docs + spaces

--- uqcsbot/scripts/wavie.py
+++ uqcsbot/scripts/wavie.py
@@ -7,15 +7,18 @@ logger = logging.getLogger(__name__)
 
 @bot.on('message')
 def wave(evt):
-	if evt.get('subtype') not in ['channel_join', 'channel_leave']:
-		return
-	channel = bot.client.server.channels.find(evt['channel'])
-	if channel.name == 'announcements':
-		return
-	result = bot.api.reactions.add(
-		name='wave',
-		channel=evt['channel'],
-		timestamp=evt['ts'],
-	)
-	if not result.get('ok'):
-		logger.error(f"Error adding reaction: {result}")
+    """
+    Wave reacts to "person joined/left this channel"
+    """
+    if evt.get('subtype') not in ['channel_join', 'channel_leave']:
+        return
+    channel = bot.client.server.channels.find(evt['channel'])
+    if channel.name == 'announcements':
+        return
+    result = bot.api.reactions.add(
+        name='wave',
+        channel=evt['channel'],
+        timestamp=evt['ts'],
+    )
+    if not result.get('ok'):
+        logger.error(f"Error adding reaction: {result}")
