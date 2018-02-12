17bd043877c04eb8a355acb99439ef583894325c
Mitchell Grice <mitch@gricey.net>
2018-02-12 11:12:54 +1000

    Improvements to !js
    Fixed bug with incorrect code escaping on exception.
    ES6.
    Made code readable

--- scripts/js.js
+++ scripts/js.js
@@ -4,21 +4,34 @@
 // Commands:
 //   `!js <JAVASCRIPT CODE>` - Returns the (safe) evaluation of the given JavaScript
 
-var safeEval = require("safe-eval");
+const safeEval = require("safe-eval");
 
 module.exports = function (robot) {
 	robot.respond(/!?(javascript|js) ?(.+)?/i, function (res) {
-        var code = res.match[2] || "('==' == '===' && '==' === '===')";
-
-        var evaluated = "You sneaky bastard! I ain't running: \r\n";
-        evaluated += "```\r\n" + code + "\r\n```";
+        const code = res.match[2] || "('==' == '===' && '==' === '===')";
+        let responseLines;
 
         try {
-            evaluated = safeEval(code);
+            const result = safeEval(code);
+            responseLines = [
+                "```",
+                code,
+                "```",
+                "evaluates to:",
+                "```",
+                result,
+                "```"
+            ];
         } catch (e) {
             // They did a bad thing!
+            responseLines = [
+                "You sneaky bastard! I ain't running:",
+                "```",
+                code,
+                "```"
+            ];
         }
 
-        res.send("```\r\n" + code + "\r\n```\r\n" + "evaluates to:" + "\r\n```\r\n" + evaluated + "\r\n```");
+        res.send(responseLines.join("\r\n");
 	});
 }
