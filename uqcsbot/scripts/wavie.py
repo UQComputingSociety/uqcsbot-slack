72238615112a0d1ea9f8dc82a3c210a67060abcb
Tom Manderson <me@trm.io>
2018-02-12 17:47:14 +1000

    Wavie.js

---
+++ uqcsbot/scripts/wavie.py
@@ -0,0 +1,21 @@
+from uqcsbot import bot
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+@bot.on('message')
+def wave(evt):
+	if evt.get('subtype') not in ['channel_join', 'channel_leave']:
+		return
+	channel = bot.client.server.channels.find(evt['channel'])
+	if channel.name == 'announcements':
+		return
+	result = bot.api.reactions.add(
+		name='wave',
+		channel=evt['channel'],
+		timestamp=evt['ts'],
+	)
+	if not result.get('ok'):
+		logger.error(f"Error adding reaction: {result}")
