96f6a3d2af56f09c161973086b1fbce6de0916e4
Connor Pane <cryan118@gmail.com>
2018-03-05 12:00:12 +1000

    Added the result link to the message

--- uqcsbot/scripts/wiki.py
+++ uqcsbot/scripts/wiki.py
@@ -6,11 +6,12 @@ import json
 @bot.on_command("wiki")
 async def handle_wiki(command: Command):
     search_query = command.arg
-    url = f"https://en.wikipedia.org/w/api.php?action=opensearch&format=json"
-    http_response = await bot.run_async(requests.get, url, params={'search': search_query})
+    api_url = f"https://en.wikipedia.org/w/api.php?action=opensearch&format=json"
+    http_response = await bot.run_async(requests.get, api_url, params={'search': search_query})
     response = json.loads(http_response.content)
 
     result_list = 2
+    url_list = 3
     best_match = 0
     first_suggestion = 1
 
@@ -22,11 +23,13 @@ async def handle_wiki(command: Command):
         return
 
     info = possible_matches[best_match]
+    url = response[url_list][best_match]
 
     # If it gives suggestions just return the first one
     suggestion_checks = ("may refer to:", "may have several meanings:")
     if any(check in info for check in suggestion_checks):
-        bot.post_message(command.channel, possible_matches[first_suggestion])
-        return
+        # Don't have to set the url as the first suggestion is still the first index in the url_list
+        info = possible_matches[first_suggestion]
 
-    bot.post_message(command.channel, info)
+    message = f'{info}\nLink: {url}'
+    bot.post_message(command.channel, message)
