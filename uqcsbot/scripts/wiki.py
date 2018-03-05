50fa1951ef0ff1c211f1dad7ac0e33b9ef445d59
Connor Pane <cryan118@gmail.com>
2018-03-05 14:28:30 +1000

    Further simplifications

--- uqcsbot/scripts/wiki.py
+++ uqcsbot/scripts/wiki.py
@@ -6,25 +6,21 @@ import json
 @bot.on_command("wiki")
 async def handle_wiki(command: Command):
     search_query = command.arg
-    api_url = f"https://en.wikipedia.org/w/api.php?action=opensearch&format=json"
+    api_url = f"https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=2"
     http_response = await bot.run_async(requests.get, api_url, params={'search': search_query})
     _, title_list, snippet_list, url_list = json.loads(http_response.content)
-
-    first_snippet = 0
-    suggestion_snippet = 1
 
     # If the results are empty let them know. Any list being empty signifies this.
     if len(title_list) == 0:
         bot.post_message(command.channel, "No Results Found.")
         return
 
-    info = snippet_list[first_snippet]
-
-    # If info is a suggestion signifier (see suggestion_checks) then use the second snippet (has content)
-    suggestion_checks = ("may refer to:", "may have several meanings:")
-    if any(check in info for check in suggestion_checks):
-        info = title_list[suggestion_snippet]
+    title, snippet, url = title_list[0], snippet_list[0], url_list[0]
+    # Detect if there are multiple references to the query, if so, use the first reference (i.e. the second item in the lists).
+    multiple_reference_instances = ("may refer to:", "may have several meanings:")
+    if any(instance in snippet for instance in multiple_reference_instances):
+        title, snippet, url = title_list[1], snippet_list[1], url_list[1]
 
     # The first url and title matches the first snippet containing any content
-    message = f'{title_list[0]}: {info}\nLink: {url}'
+    message = f'{title}: {snippet}\nLink: {url}'
     bot.post_message(command.channel, message)
