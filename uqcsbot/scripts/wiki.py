9e3ca9f7626e7b91d06ef845e048e36cdbc06f82
Connor <cryan118@gmail.com>
2018-03-05 18:03:41 +1000

    Added a check for rare cases where a result was an empty string

--- uqcsbot/scripts/wiki.py
+++ uqcsbot/scripts/wiki.py
@@ -11,6 +11,7 @@ async def handle_wiki(command: Command):
     http_response = await bot.run_async(requests.get, api_url, params={'search': search_query})
     if http_response.status_code != requests.codes.ok:
         bot.post_message(command.channel, "Problem fetching data")
+        return
 
     _, title_list, snippet_list, url_list = json.loads(http_response.content)
 
@@ -20,9 +21,15 @@ async def handle_wiki(command: Command):
         return
 
     title, snippet, url = title_list[0], snippet_list[0], url_list[0]
+
+    # Sometimes the first element is an empty string which is weird so we handle that rare case here
+    if '' in (title, snippet, url):
+        bot.post_message(command.channel, "Sorry, there was something funny about the result")
+        return
+
     # Detect if there are multiple references to the query, if so, use the first reference (i.e. the second item in the lists).
     multiple_reference_instances = ("may refer to:", "may have several meanings:")
-    if any(instance in snippet for instance in multiple_reference_instances) or not snippet:
+    if any(instance in snippet for instance in multiple_reference_instances):
         title, snippet, url = title_list[1], snippet_list[1], url_list[1]
 
     # The first url and title matches the first snippet containing any content
