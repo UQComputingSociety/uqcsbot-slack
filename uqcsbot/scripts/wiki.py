b883c76b24daaf45ce6b810bff00ae6b637e38f5
Connor Pane <cryan118@gmail.com>
2018-03-05 14:23:33 +1000

    Removed unnecessary indexes into lists

--- uqcsbot/scripts/wiki.py
+++ uqcsbot/scripts/wiki.py
@@ -8,28 +8,23 @@ async def handle_wiki(command: Command):
     search_query = command.arg
     api_url = f"https://en.wikipedia.org/w/api.php?action=opensearch&format=json"
     http_response = await bot.run_async(requests.get, api_url, params={'search': search_query})
-    response = json.loads(http_response.content)
+    _, title_list, snippet_list, url_list = json.loads(http_response.content)
 
-    result_list = 2
-    url_list = 3
-    best_match = 0
-    first_suggestion = 1
+    first_snippet = 0
+    suggestion_snippet = 1
 
-    possible_matches = response[result_list]
-
-    # If the results are empty let them know
-    if not possible_matches:
-        bot.post_message(command.channel, "No Results.")
+    # If the results are empty let them know. Any list being empty signifies this.
+    if len(title_list) == 0:
+        bot.post_message(command.channel, "No Results Found.")
         return
 
-    info = possible_matches[best_match]
-    url = response[url_list][best_match]
+    info = snippet_list[first_snippet]
 
-    # If it gives suggestions just return the first one
+    # If info is a suggestion signifier (see suggestion_checks) then use the second snippet (has content)
     suggestion_checks = ("may refer to:", "may have several meanings:")
     if any(check in info for check in suggestion_checks):
-        # Don't have to set the url as the first suggestion is still the first index in the url_list
-        info = possible_matches[first_suggestion]
+        info = title_list[suggestion_snippet]
 
-    message = f'{info}\nLink: {url}'
+    # The first url and title matches the first snippet containing any content
+    message = f'{title_list[0]}: {info}\nLink: {url}'
     bot.post_message(command.channel, message)
