4b0cc50efc7a06d25870084d0b1288c955e89531
Connor Pane <cryan118@gmail.com>
2018-03-05 11:47:18 +1000

    More descriptive variables and replaced try except with if statement

--- uqcsbot/scripts/wiki.py
+++ uqcsbot/scripts/wiki.py
@@ -5,22 +5,28 @@ import json
 
 @bot.on_command("wiki")
 async def handle_wiki(command: Command):
-    search = command.arg
-    url = f"https://en.wikipedia.org/w/api.php?action=opensearch&search={search}&format=json"
-    http_response = await bot.run_async(requests.get, url)
+    search_query = command.arg
+    url = f"https://en.wikipedia.org/w/api.php?action=opensearch&format=json"
+    http_response = await bot.run_async(requests.get, url, params={'search': search_query})
     response = json.loads(http_response.content)
 
+    result_list = 2
+    best_match = 0
+    first_suggestion = 1
+
+    possible_matches = response[result_list]
+
     # If the results are empty let them know
-    try:
-        info = response[2][0]
-    except IndexError:
-        bot.post_message(command.channel, "No Results.\r\n")
+    if not possible_matches[best_match]:
+        bot.post_message(command.channel, "No Results.")
         return
 
+    info = possible_matches[best_match]
+
     # If it gives suggestions just return the first one
-    typical = "may refer to:"
-    if typical in info:
-        bot.post_message(command.channel, response[2][1])
+    suggestion_checks = ("may refer to:", "may have several meanings:")
+    if any(check in info for check in suggestion_checks):
+        bot.post_message(command.channel, possible_matches[first_suggestion])
         return
 
     bot.post_message(command.channel, info)
