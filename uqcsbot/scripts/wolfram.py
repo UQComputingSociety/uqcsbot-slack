31878a8db3928d7664f4e6bdd720a533e9714c00
mubiquity <cryan118@gmail.com>
2018-03-08 21:51:09 +1000

    Cleaned up code a bit

--- uqcsbot/scripts/wolfram.py
+++ uqcsbot/scripts/wolfram.py
@@ -5,7 +5,6 @@ import json
 
 APP_ID = "UVKV2V-5XW2TETT69"
 
-
 # IDEA: Show the assumptions made?
 # TODO: Better naming of commands
 @bot.on_command("wolframfull")
@@ -72,7 +71,7 @@ async def handle_wolfram(command: Command):
         error = wolfram_result['error']
         if error == "No result is available":
             # If no conversational result is available just return a normal short answer
-            short_response = await short_answer()
+            short_response = await short_answer(search_query)
             bot.post_message(command.channel, short_response)
             return
         else:
@@ -82,8 +81,8 @@ async def handle_wolfram(command: Command):
     # Unpack the response in order to construct the reply message
     result = wolfram_result['result']  # This is the answer to our question
     reply_host = wolfram_result['host']  # This is the hostname to ask the next question to
-    conversation_id = wolfram_result['conversationID']  # This is the conversations identifier. Used to continue the conversation
-    s_output = wolfram_result.get('s', None)  # s is only sometimes returned but is vital if it is returned (no idea what is stands for)
+    conversation_id = wolfram_result['conversationID']  # Used to continue the conversation
+    s_output = wolfram_result.get('s', None)  # s is only sometimes returned but is vital if it is returned
 
     # TODO: Is there a better option than storing the id in the fallback?
     # Here we store the conversation ID in the fallback so we can get it back later.
@@ -96,37 +95,37 @@ async def handle_wolfram(command: Command):
 
     bot.post_message(command.channel, "", attachments=attachments)
 
-# TODO: Add 'ok' checks
+
 @bot.on('message')
 async def handle_reply(evt: dict):
     # If the message isn't from a thread or is from a bot ignore it (avoid those infinite loops)
-    if 'thread_ts' not in evt or ('subtype' in evt and evt['subtype'] == 'bot_message'):
+    if 'thread_ts' not in evt or (evt.get('subtype') == 'bot_message'):
         return
 
     channel = evt['channel']
-
-    thread_ts = evt['thread_ts']
+    thread_ts = evt['thread_ts']  # This refers to time the original message
     thread_parent = bot.api.conversations.history(channel=channel, limit=1, inclusive=True, latest=thread_ts)
 
     if not thread_parent['ok']:
         bot.post_message(channel, 'Sorry, something went wrong with slack', thread_ts=thread_ts)
         return
 
-    parent_message = thread_parent['messages'][0]
-    # If the threads parent wasn't by a bot ignore
-    if 'subtype' not in parent_message or parent_message['subtype'] != 'bot_message':
+    parent_message = thread_parent['messages'][0]  # Limit=1 was used so the first (and only) message is what we want
+    # If the threads parent wasn't by a bot ignore it
+    if parent_message.get('subtype') != 'bot_message':
         return
 
     # Finally, we have to check that this is a Wolfram replyable message
-    # It is rare we would reach this point and not pass as who replies to a bot in a thread for another reason
-    parent_attachments = parent_message['attachments'][0]
+    # It is rare we would reach this point and not pass as who replies to a bot in a thread for another reason?
+    parent_attachments = parent_message['attachments'][0]  # The message is formatted with only one attachment
     parent_fallback = parent_attachments['fallback']
     if 'WolframCanReply' not in parent_fallback:
         return
 
     # Now we can grab the conversation_id from the message and get the new question (s only sometimes appears)
+    # Recall the format of the fallback "identifier hostname s_output conversationID"
     _, reply_host, s_output, conversation_id = parent_fallback.split(' ')
-    new_question = evt['text']
+    new_question = evt['text']  # This is the value of the message that triggered this whole response
     s_output = '' if s_output == 'None' else s_output
 
     # Slack annoyingly formats the reply_host link so we have to extract what we want:
@@ -142,27 +141,26 @@ async def handle_reply(evt: dict):
         return
 
     # Convert to json and check for an error
-    json_response = json.loads(http_response.content)
-    if 'error' in json_response:
-        bot.post_message(channel, json_response['error'], thread_ts=thread_ts)
+    wolfram_answer = json.loads(http_response.content)
+    if 'error' in wolfram_answer:
+        bot.post_message(channel, wolfram_answer['error'], thread_ts=thread_ts)
         return
 
     # Otherwise grab the new stuff and post the reply.
-    reply = json_response['result']
-    conversation_id = json_response['conversationID']
-    reply_host = json_response['host']
-    s_output = json_response.get('s', None)
+    reply = wolfram_answer['result']
+    conversation_id = wolfram_answer['conversationID']
+    reply_host = wolfram_answer['host']
+    s_output = wolfram_answer.get('s', None)
 
     bot.post_message(channel, reply, thread_ts=thread_ts)
 
-    # Update the old conversation_id to indicate the new state
-    updated_attachments = parent_attachments.copy()
-    updated_attachments['fallback'] = f'WolframCanReply {reply_host} {s_output} {conversation_id}'
+    # Update the old fallback to reflect the new state of the conversation
+    parent_attachments['fallback'] = f'WolframCanReply {reply_host} {s_output} {conversation_id}'
 
-    bot.api.chat.update(channel=channel, attachments=[updated_attachments], ts=thread_ts)
+    bot.api.chat.update(channel=channel, attachments=[parent_attachments], ts=thread_ts)
 
 
-async def short_answer():
+async def short_answer(search_query: str):
     """
     This uses wolfram's short answers api to just return a simple short plaintext response.
 
@@ -170,18 +168,16 @@ async def short_answer():
     conversation starter but may be interesting.
     """
     api_url = r"http://api.wolframalpha.com/v1/result?"
-    search_query = command.arg
     http_response = await bot.run_async(requests.get, api_url, params={'input': search_query, 'appid': APP_ID})
 
     # Check if the response is ok. A status code of 501 signifies that no result could be found.
     if http_response.status_code == 501:
-        bot.post_message(command.channel, "No short answer available. Try !wolframfull")
-        return
+        return "No short answer available. Try !wolframfull"
     elif http_response.status_code != requests.codes.ok:
-        bot.post_message(command.channel, "There was a problem getting the response")
-        return
+        return "There was a problem getting the response"
 
     return http_response.content
+
 
 def get_subpods(pods: list) -> Iterable[Tuple[str, dict]]:
     """
