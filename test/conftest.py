b67c4d0edb345f9f81508bc80f0595cf71919621
mitchmcdee <mitchmcdee@gmail.com>
2018-04-18 16:20:19 +1000

    More code cleanup

--- test/conftest.py
+++ test/conftest.py
@@ -20,10 +20,6 @@ TEST_GROUP_ID = "G1234567890"
 TEST_DIRECT_ID = "D1234567890"
 TEST_USER_ID = "U1234567890"
 
-def generate_channel(channel_id, name, is_group, is_im, is_user_deleted, is_public, is_private, is_archived, users):
-    return {'id': channel_id, 'name': name, 'is_group': is_group, 'is_im': is_im, 'is_user_deleted': is_user_deleted,
-            'is_public': is_public, 'is_private': is_private, 'is_archived': is_archived, 'users': users}
-
 
 class UnparsedCommandException(Exception):
     """
@@ -32,7 +28,6 @@ class UnparsedCommandException(Exception):
     """
     pass
 
-
 class UnmatchedHandleException(Exception):
     """
     Unlike the actual bot, a message which doesn't match any handlers will raise this exception rather than being ignored
@@ -40,18 +35,20 @@ class UnmatchedHandleException(Exception):
     """
     pass
 
+def generate_channel(channel_id, name, users=[], is_group=False, is_im=False, is_user_deleted=False, is_public=False,
+                     is_private=False, is_archived=False):
+    return {'id': channel_id, 'name': name, 'is_group': is_group, 'is_im': is_im, 'is_user_deleted': is_user_deleted,
+            'is_public': is_public, 'is_private': is_private, 'is_archived': is_archived, 'users': users}
+
 class MockUQCSBot(UQCSBot):
     test_posted_messages = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.test_posted_messages = defaultdict(deque)
-        self.test_channels = {TEST_CHANNEL_ID: generate_channel(TEST_CHANNEL_ID, TEST_CHANNEL_ID, False, False, False,
-                                                                True, False, False, [TEST_USER_ID]),
-                               TEST_GROUP_ID: generate_channel(TEST_GROUP_ID, TEST_GROUP_ID, True, False, False, False,
-                                                               True, False, [TEST_USER_ID]),
-                               TEST_DIRECT_ID: generate_channel(TEST_DIRECT_ID, TEST_DIRECT_ID, False, True, False,
-                                                                False, True, False, [TEST_USER_ID])}
+        self.test_channels = {TEST_CHANNEL_ID: generate_channel(TEST_CHANNEL_ID, TEST_CHANNEL_ID, [TEST_USER_ID], is_public=True),
+                               TEST_GROUP_ID: generate_channel(TEST_GROUP_ID, TEST_GROUP_ID, [TEST_USER_ID], is_group=True, is_private=True),
+                               TEST_DIRECT_ID: generate_channel(TEST_DIRECT_ID, TEST_DIRECT_ID, [TEST_USER_ID], is_im=True, is_private=True)}
 
         def mocked_api_call(method, **kwargs):
             if method == 'channels.list':
@@ -79,30 +76,36 @@ class MockUQCSBot(UQCSBot):
         channel_id = kwargs.get('channel')
         cursor = kwargs.get('cursor', 0)
         limit = kwargs.get('limit', 100)
+
         channel = self.test_channels.get(channel_id)
         if channel is None:
             return {'ok', False}
+
         all_users = channel.get('users')
         sliced_users = all_users[cursor : cursor + limit + 1]
         cursor += len(sliced_users)
         if cursor == len(all_users):
             cursor = None
+
         return {'ok': True, 'members': sliced_users, 'cursor': cursor}
 
     def conversations_history(self, **kwargs):
         channel_id = kwargs.get('channel')
         cursor = kwargs.get('cursor', 0)
         limit = kwargs.get('limit', 100)
+
         all_messages = self.test_posted_messages.get(channel_id, [])
         sliced_messages = list(islice(all_messages, cursor, cursor + limit + 1))
         cursor += len(sliced_messages)
         if cursor == len(all_messages):
             cursor = None
+
         return {'ok': True, 'messages': sliced_messages, 'cursor': cursor}
 
     def channel_list(self, channel_type=None, **kwargs):
         cursor = kwargs.get('cursor', 0)
         limit = kwargs.get('limit', 100)
+
         if channel_type == 'channels':
             filter_function = lambda x: x.get('is_public', False)
         elif channel_type == 'groups':
@@ -111,11 +114,13 @@ class MockUQCSBot(UQCSBot):
             filter_function = lambda x: x.get('is_im', False)
         else:
             filter_function = lambda *_: True
+
         all_channels = list(filter(filter_function, self.test_channels.values()))
         sliced_channels = all_channels[cursor : cursor + limit + 1]
         cursor += len(sliced_channels)
         if cursor == len(all_channels):
             cursor = None
+
         return {'ok': True, channel_type: sliced_channels, 'cursor': cursor}
 
     def post_message(self, channel: Union[Channel, str], text: str, **kwargs):
@@ -126,7 +131,6 @@ class MockUQCSBot(UQCSBot):
     async def post_and_handle_command(self, message):
         self.post_message(message['channel'], message['text'])
         command = Command.from_message(self, message)
-        print(command)
         if command is None:
             raise UnparsedCommandException()
         if command.command_name not in self._command_registry:
