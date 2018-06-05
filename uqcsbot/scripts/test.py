18de3e76c21dc1fa68db37428011e1b84cdb41b3
mitchmcdee <mitchmcdee@gmail.com>
2018-06-05 18:01:16 +1000

    uqcsbot: oops removed test script

--- uqcsbot/scripts/test.py
+++
@@ -1,7 +0,0 @@
-from uqcsbot import bot, Command
-
-
-@bot.on_command("test")
-def handle_test(command: Command):
-    for page in bot.api.conversations.members.paginate(limit=1, channel=command.channel.id):
-        print(page.get('members'))
