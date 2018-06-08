49128ce72fe39c333babf19935392713ca1bc62c
mitchmcdee <mitchmcdee@gmail.com>
2018-06-08 14:03:07 +1000

    events: added date type detection and datetime conversion

--- uqcsbot/scripts/events.py
+++ uqcsbot/scripts/events.py
@@ -2,7 +2,7 @@ from typing import List
 from uqcsbot import bot, Command
 from icalendar import Calendar, vText
 import requests
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from pytz import timezone, utc
 import re
 
@@ -69,6 +69,13 @@ class Event(object):
     def from_cal_event(cls, cal_event):
         start = cal_event.get('dtstart').dt
         end = cal_event.get('dtend').dt
+        # ical 'dt' properties are parsed as a 'DDD' (datetime, date, duration)
+        # type. The below code converts a date to a datetime, where time is set
+        # to midnight.
+        if isinstance(start, date):
+            start = datetime.combine(start, datetime.min.time()).astimezone(utc)
+        if isinstance(end, date):
+            end = datetime.combine(end, datetime.min.time()).astimezone(utc)
         location = cal_event.get('location', 'TBA')
         summary = cal_event.get('summary')
         return cls(start, end, location, summary)
