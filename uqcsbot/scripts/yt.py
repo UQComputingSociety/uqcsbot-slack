4ef585a1168e8fc45cff5f64853096b200deec72
Jack Caperon <jack.d.caperon@gmail.com>
2018-06-08 22:21:20 +1000

    Added tests for yt normal and testing no query

--- uqcsbot/scripts/yt.py
+++ uqcsbot/scripts/yt.py
@@ -5,31 +5,37 @@ import argparse
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+YOUTUBE_API_KEY = 'AIzaSyCtxVMs6So6x2WL5WkDV4rm01hzddCCGH4'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v='
 
+
 @bot.on_command('yt')
-async def handle_yt(command: Command):
+def handle_yt(command: Command):
     '''
     `!yt <QUERY>` - Returns the top search result based on the query string.
+    
+    handle_yt checks the usage of yt command and appropriately selects logic.
     '''
     # Makes sure the query is not empty.
     if command.has_arg():
         try:
             cmd = command.arg.strip()
-            await yt_normal(cmd, command.channel)
+            yt_normal(cmd, command.channel)
         except HttpError as e:
             # Googleapiclient should handle http errors
             print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
     else:
-        bot.post_message(command.channel, "You can't look for nothing.")
+        bot.post_message(command.channel, "You can't look for nothing. !yt <QUERY>")
         
-async def yt_normal(search_query: str, channel):
-    # The normal method for using !yt searches based on query
-    # and returns the first result. "I'm feeling lucky"
+def yt_normal(search_query: str, channel):
+    '''
+    The normal method for using !yt searches based on query
+    and returns the first result. "I'm feeling lucky"
+    '''
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-        developerKey=os.environ['YOUTUBE_API_KEY'])
+        developerKey=YOUTUBE_API_KEY)
 
     search_response = youtube.search().list(
         q=search_query,
