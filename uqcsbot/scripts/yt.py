7ba2999f765570f38ad4aa7eb8f8e1c17d1d803f
Jack Caperon <jack.d.caperon@gmail.com>
2018-06-09 10:32:30 +1000

    Fixed code styling errors and refactored bot messaging to parent functions. Also switched to using environment API keys.

--- uqcsbot/scripts/yt.py
+++ uqcsbot/scripts/yt.py
@@ -4,6 +4,7 @@ from uqcsbot import bot, Command
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY')
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v='
@@ -12,26 +13,31 @@ YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v='
 @bot.on_command('yt')
 def handle_yt(command: Command):
     '''
-    `!yt <QUERY>` - Returns the top search result based on the query string.
+    `!yt <QUERY>` - Returns the top video search result based on the query string.
     '''
     # Makes sure the query is not empty.
     if command.has_arg():
+        search_query = command.arg.strip()
         try:
-            cmd = command.arg.strip()
-            yt_normal(cmd, command.channel)
+            videoID = get_top_video_result(search_query, command.channel)
         except HttpError as e:
             # Googleapiclient should handle http errors
-            print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+            bot.logger.error(f'An HTTP error {e.resp.status} occurred:\n{e.content}')
     else:
         bot.post_message(command.channel, "You can't look for nothing. !yt <QUERY>")
+    
+    if videoID:
+        bot.post_message(command.channel, f'{YOUTUBE_VIDEO_URL}{videoID}')
+    else:
+        bot.post_message(command.channel, "Your query returned no results.")
         
-def yt_normal(search_query: str, channel):
+def get_top_video_result(search_query: str, channel):
     '''
     The normal method for using !yt searches based on query
     and returns the first video result. "I'm feeling lucky"
     '''
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-        developerKey=os.environ.get('YOUTUBE_API_KEY'))
+                    developerKey=YOUTUBE_API_KEY)
 
     search_response = youtube.search().list(
         q=search_query,
@@ -40,11 +46,9 @@ def yt_normal(search_query: str, channel):
         type='video' # Only want videos no pesky channels or playlists
     ).execute()
     
-    search_result = search_response.get('items',[])
+    search_result = search_response.get('items', [])
 
     if len(search_result):
-        videoId = search_result[0]['id']['videoId']
-        message = YOUTUBE_VIDEO_URL+videoId
-        bot.post_message(channel, message)
+        return search_result[0]['id']['videoId']
     else:
-        bot.post_message(channel, "Your query returned no results.")
+        return None
