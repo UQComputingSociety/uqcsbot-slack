2b699e6350536641a5406f38b19ea1db8edee09b
mitchmcdee <mitchmcdee@gmail.com>
2018-06-15 02:30:35 +1000

    tests: tidied up tests

--- test/test_dog.py
+++ test/test_dog.py
@@ -1,6 +1,10 @@
 from test.conftest import MockUQCSBot, TEST_CHANNEL_ID
 from test.helpers import generate_message_object
 
+# TODO(mitch): work out a way to get the dog from dog.py without triggering
+# 'on_command' to be called and add '!dog' as a handler which messes with
+# testing.
+
 def test_dog(uqcsbot: MockUQCSBot):
     '''
     test !dog
--- test/test_mock.py
+++ test/test_mock.py
@@ -1,18 +1,34 @@
 from test.conftest import MockUQCSBot, TEST_CHANNEL_ID
 from test.helpers import generate_message_object
 
-def test_mock(uqcsbot: MockUQCSBot):
+LONG_MESSAGE = 'I\'m going to say a really long sentence that has a really low'\
+               + ' probability of outputting a random-case mocked message that'\
+               + ' is equal to the original.'
+
+def test_mock_previous_message(uqcsbot: MockUQCSBot):
     '''
-    test !mock
+    Test !mock on the immediately previous message
     '''
-    message = 'I\'m going to say a really long sentence that has a really low' \
-              + ' probability of outputting a random-case mocked message that' \
-              + ' is equal to the original.'
-    uqcsbot.post_message(TEST_CHANNEL_ID, message)
+    uqcsbot.post_message(TEST_CHANNEL_ID, LONG_MESSAGE)
     mock_call = generate_message_object(TEST_CHANNEL_ID, '!mock')
-    # TODO(mitch): add !mock 3 test
     uqcsbot.post_and_handle_command(mock_call)
     channel_messages = uqcsbot.test_posted_messages.get(TEST_CHANNEL_ID, [])
     assert len(channel_messages) == 3
-    assert channel_messages[0]['text'].lower() == message.lower()
-    assert channel_messages[0]['text'] != message
+    assert channel_messages[0]['text'].lower() == LONG_MESSAGE.lower()
+    assert channel_messages[0]['text'] != LONG_MESSAGE
+
+def test_mock_past_message(uqcsbot: MockUQCSBot):
+    '''
+    Test !mock on a message from the past
+    '''
+    uqcsbot.post_message(TEST_CHANNEL_ID, LONG_MESSAGE)
+    uqcsbot.post_message(TEST_CHANNEL_ID, 'just')
+    uqcsbot.post_message(TEST_CHANNEL_ID, 'some')
+    uqcsbot.post_message(TEST_CHANNEL_ID, 'message')
+    uqcsbot.post_message(TEST_CHANNEL_ID, 'padding')
+    mock_call = generate_message_object(TEST_CHANNEL_ID, '!mock 4')
+    uqcsbot.post_and_handle_command(mock_call)
+    channel_messages = uqcsbot.test_posted_messages.get(TEST_CHANNEL_ID, [])
+    assert len(channel_messages) == 7
+    assert channel_messages[0]['text'].lower() == LONG_MESSAGE.lower()
+    assert channel_messages[0]['text'] != LONG_MESSAGE
