1862018810014337b3818bab0e17901ded3f0586
mitchmcdee <mitchmcdee@gmail.com>
2018-06-15 16:10:00 +1000

    tests: tidied up welcome test

--- test/test_welcome.py
+++ test/test_welcome.py
@@ -3,6 +3,11 @@ from test.helpers import (generate_event_object, MESSAGE_TYPE_CHANNEL_CREATED,
                           MESSAGE_TYPE_MEMBER_JOINED_CHANNEL)
 from unittest.mock import patch
 
+# TODO(mitch): replace this with the milestone number once you've worked out how
+# to import files without triggering 'on_command' to be called which messes with
+# testing.
+MEMBER_MILESTONE = 50
+
 def test_welcome(uqcsbot: MockUQCSBot):
     '''
     Test !welcome for a member joining UQCS.
@@ -32,10 +37,6 @@ def test_welcome_milestone(uqcsbot: MockUQCSBot):
     '''
     Test !welcome for a member joining on a milestone number.
     '''
-    # TODO(mitch): replace this with the milestone number once you've worked out
-    # how to import files without triggering 'on_command' to be called which
-    # messes with testing.
-    NUM_JOINED_MEMBERS = 50
     events = [
         generate_event_object(MESSAGE_TYPE_CHANNEL_CREATED,
                               channel={'id': 'general', 'name': 'general',
@@ -44,7 +45,7 @@ def test_welcome_milestone(uqcsbot: MockUQCSBot):
                               channel={'id': 'announcements', 'name': 'announcements',
                                        'is_public': True}),
     ]
-    for i in range(NUM_JOINED_MEMBERS):
+    for _ in range(MEMBER_MILESTONE):
         events.append(
             generate_event_object(MESSAGE_TYPE_MEMBER_JOINED_CHANNEL,
                                   channel='announcements', user=TEST_USER_ID)
@@ -55,5 +56,5 @@ def test_welcome_milestone(uqcsbot: MockUQCSBot):
             uqcsbot._run_handlers(event)
 
     general_messages = uqcsbot.test_messages.get('general', [])
-    assert len(general_messages) == NUM_JOINED_MEMBERS + 1
-    assert general_messages[-1]['text'] == f':tada: {NUM_JOINED_MEMBERS} members! :tada:'
+    assert len(general_messages) == MEMBER_MILESTONE + 1
+    assert general_messages[-1]['text'] == f':tada: {MEMBER_MILESTONE} members! :tada:'
