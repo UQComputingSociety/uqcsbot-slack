e840e3ed55728cdb3e761e773f1afcadf0b0992d
mitchmcdee <mitchmcdee@gmail.com>
2018-06-17 00:11:32 +1000

    id: added id command. fixed mocked post_message to support users

---
+++ uqcsbot/scripts/id.py
@@ -0,0 +1,8 @@
+from uqcsbot import bot, Command
+
+@bot.on_command("id")
+def handle_id(command: Command):
+    '''
+    `!id` - Returns the calling user's Slack ID.
+    '''
+    bot.post_message(command.channel_id, command.user_id)
