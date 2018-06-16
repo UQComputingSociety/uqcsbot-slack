5b0392046c172e39a45c5b5c70cc5b59944985e5
Nicholas Lambourne <ndl93@live.com>
2018-06-16 20:56:20 +1000

    feedback and removed js

--- uqcsbot/scripts/urban.py
+++ uqcsbot/scripts/urban.py
@@ -28,17 +28,16 @@ def handle_urban(command: Command) -> None:
 
     # If search phrase is not found, notify user.
     if results['result_type'] != 'exact':
-        bot.post_message(command.channel, '> No results found for ' + search_term + '. ¯\\_(ツ)_/¯')
+        bot.post_message(command.channel, f'> No results found for {search_term}. ¯\\_(ツ)_/¯')
         return
 
     # Get best definition (by most 'thumbs up') and construct response.
     sorted_definitions = sorted(results['list'], key=lambda definition: definition['thumbs_up'], reverse=True)
     best_definition = sorted_definitions[0]
-    response_example = best_definition.get('example', '')
-    more_definitions = '_ more definitions at ' + http_response.url.replace(URBAN_API_ENDPOINT, URBAN_USER_ENDPOINT) +\
-                       ' _' if len(sorted_definitions) > 1 else ''
+    response_example = best_definition.get('example', '').replace('\r\n', '\n> ')  # Format example with block quotes.
+    more_definitions = f'_ more definitions at {http_response.url.replace(URBAN_API_ENDPOINT, URBAN_USER_ENDPOINT)} _'
 
-    # Format and send response to user in channel query was sent from.
-    bot.post_message(command.channel, '*' + search_term.title() + '* \n ' +
-                     best_definition['definition'].capitalize() +
-                     '\n> ' + response_example.replace('\r\n', '\n> ') + '\n' + more_definitions)
+    # Format message and send response to user in channel query was sent from.
+    message = f'*{search_term.title()}*\n{best_definition["definition"].capitalize()}\n> ' \
+              f'{response_example}\n{more_definitions if len(sorted_definitions) > 1 else ""}'
+    bot.post_message(command.channel, message)
