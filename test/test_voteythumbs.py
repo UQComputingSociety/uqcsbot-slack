dd7202db215ca6cd7fed1916172be05142623f42
mitchmcdee <mitchmcdee@gmail.com>
2018-06-17 20:26:09 +1000

    voteythumbs: removed invalid usage test

--- test/test_voteythumbs.py
+++ test/test_voteythumbs.py
@@ -2,7 +2,7 @@ from test.conftest import MockUQCSBot, TEST_CHANNEL_ID
 from uqcsbot.utils.command_utils import get_helper_doc
 
 
-def test_valid_voteythumbs(uqcsbot: MockUQCSBot):
+def test_voteythumbs(uqcsbot: MockUQCSBot):
     '''
     Tests !voteythumbs
     '''
@@ -10,14 +10,3 @@ def test_valid_voteythumbs(uqcsbot: MockUQCSBot):
     messages = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
     # Make sure there are three reactions (thumbsup, thumbsdown, eyes)
     assert len(messages[-1]['reactions']) == 3
-
-
-def test_invalid_voteythumbs(uqcsbot: MockUQCSBot):
-    '''
-    Tests that the bot correctly handles an invalid !voteythumbs.
-    '''
-    uqcsbot.post_message(TEST_CHANNEL_ID, '!voteythumbs')
-    voteythumbs_doc = get_helper_doc('voteythumbs')
-    messages = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
-    assert len(messages) == 2
-    assert messages[-1]['text'] == f'usage: {voteythumbs_doc}'
