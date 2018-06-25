45ccfe55debc7a3d50c97dacdf05cee8aa04d93e
mitchmcdee <mitchmcdee@gmail.com>
2018-06-25 20:28:42 +1000

    whatsdue: removed whatsdue from this PR

--- uqcsbot/scripts/whatsdue.py
+++ uqcsbot/scripts/whatsdue.py
@@ -1,7 +1,6 @@
-import argparse
 from datetime import datetime
 from uqcsbot import bot, Command
-from uqcsbot.utils.command_utils import loading_status, UsageSyntaxException
+from uqcsbot.utils.command_utils import loading_status
 from uqcsbot.utils.uq_course_utils import (get_course_assessment,
                                            HttpException,
                                            CourseNotFoundException,
@@ -27,27 +26,30 @@ def handle_whatsdue(command: Command):
     `!whatsdue [-f] [--full] [COURSE CODE 1] [COURSE CODE 2] ...` - Returns all
     the assessment for a given list of course codes that are scheduled to occur
     after today. If unspecified, will attempt to return the assessment for the
-    channel that the command was called from. If `-f`/`--full` is provided, will
+    channel that the command was called from. If -f/--full is provided, will
     return the full assessment list without filtering by cutoff dates.
     '''
     channel = bot.channels.get(command.channel_id)
     command_args = command.arg.split() if command.has_arg() else []
 
-    arg_parser = argparse.ArgumentParser()
-    def usage_error(*args, **kwargs):
-        raise UsageSyntaxException()
-    arg_parser.error = usage_error  # type: ignore
-    arg_parser.add_argument('-f', '--full', action='store_true')
-    arg_parser.add_argument('course_codes', nargs='*')
+    is_full_output = False
+    if '--full' in command_args:
+        command_args.remove('--full')
+        is_full_output = True
+    if '-f' in command_args:
+        command_args.remove('-f')
+        is_full_output = True
 
-    parsed_args = arg_parser.parse_args(command_args)
-    course_names = parsed_args.course_codes if len(parsed_args.course_codes) > 0 else [channel.name]
+    # If we have any command args left, they're course names. If we don't,
+    # attempt to instead use the channel name as the course name.
+    course_names = command_args if len(command_args) > 0 else [channel.name]
+
     if len(course_names) > COURSE_LIMIT:
         bot.post_message(channel, f'Cannot process more than {COURSE_LIMIT} courses.')
         return
 
     # If full output is not specified, set the cutoff to today's date.
-    cutoff = None if parsed_args.full else datetime.today()
+    cutoff = None if is_full_output else datetime.today()
     try:
         assessment = get_course_assessment(course_names, cutoff)
     except HttpException as e:
@@ -61,7 +63,7 @@ def handle_whatsdue(command: Command):
     message = '_*WARNING:* Assessment information may vary/change/be entirely' \
               + ' different! Use at your own discretion_\n>>>'
     message += '\n'.join(map(get_formatted_assessment_item, assessment))
-    if not parsed_args.full:
+    if not is_full_output:
         message += '\n_Note: This may not be the full assessment list. Use -f' \
                    + '/--full to print out the full list._'
     bot.post_message(channel, message)
