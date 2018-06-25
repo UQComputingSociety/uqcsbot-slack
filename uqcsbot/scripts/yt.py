3a368342a76350ab5237459e07713ce217c0223d
mitchmcdee <mitchmcdee@gmail.com>
2018-06-25 20:38:45 +1000

    yt: removed yt from this PR

--- uqcsbot/scripts/yt.py
+++ uqcsbot/scripts/yt.py
@@ -1,19 +1,44 @@
 import os
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from uqcsbot import bot, Command
 from uqcsbot.utils.command_utils import UsageSyntaxException
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY')
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v='
+NO_QUERY_MESSAGE = "You can't look for nothing. !yt <QUERY>"
 
 
-def get_top_video_result(search_query: str):
+@bot.on_command('yt')
+def handle_yt(command: Command):
     '''
-    The normal method for using !yt searches based on query and returns the
-    first video result akin to a "I'm feeling lucky" search.
+    `!yt <QUERY>` - Returns the top video search result based on the query string.
+    '''
+    # Makes sure the query is not empty.
+    if not command.has_arg():
+        raise UsageSyntaxException()
+
+    search_query = command.arg.strip()
+    try:
+        videoID = get_top_video_result(search_query, command.channel_id)
+    except HttpError as e:
+        # Googleapiclient should handle http errors
+        bot.logger.error(f'An HTTP error {e.resp.status} occurred:\n{e.content}')
+        # Force return to ensure no message is sent.
+        return
+
+    if videoID:
+        bot.post_message(command.channel_id, f'{YOUTUBE_VIDEO_URL}{videoID}')
+    else:
+        bot.post_message(command.channel_id, "Your query returned no results.")
+
+
+def get_top_video_result(search_query: str, channel):
+    '''
+    The normal method for using !yt searches based on query
+    and returns the first video result. "I'm feeling lucky"
     '''
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                     developerKey=YOUTUBE_API_KEY)
@@ -29,25 +54,3 @@ def get_top_video_result(search_query: str):
     if search_result is None:
         return None
     return search_result[0]['id']['videoId']
-
-
-@bot.on_command('yt')
-def handle_yt(command: Command):
-    '''
-    `!yt <QUERY>` - Returns the top video search result based on the query string.
-    '''
-    if not command.has_arg():
-        raise UsageSyntaxException()
-
-    search_query = command.arg.strip()
-    try:
-        video_id = get_top_video_result(search_query)
-    except HttpError as e:
-        bot.logger.error(f'An HTTP error {e.resp.status} occurred:\n{e.content}')
-        return
-
-    if video_id is None:
-        message = "Your query returned no results."
-    else:
-        message = f'{YOUTUBE_VIDEO_URL}{video_id}'
-    bot.post_message(command.channel_id, message)
