283bf48c0dec58fd8b553eb344b0d2bc1fe4d789
mitchmcdee <mitchmcdee@gmail.com>
2018-06-25 15:05:32 +1000

    caesar: removed caesar from this PR

--- uqcsbot/scripts/caesar.py
+++ uqcsbot/scripts/caesar.py
@@ -1,34 +1,32 @@
-import argparse
-from uqcsbot import bot, Command
-from uqcsbot.utils.command_utils import UsageSyntaxException
+import re
+from uqcsbot import bot
+
+CAESAR_REGEX = re.compile('!caesar(|-?\d+) (.+)')
 
 
-@bot.on_command('caesar')
-def handle_caesar(command: Command):
+@bot.on('message')
+def handle_caesar(message: dict):
     '''
-    `!caesar [<-n> <NUM_SHIFT>] <TEXT>` - Performs caesar shift with a left
-    shift of NUM_SHIFT on the given text. If NUM_SHIFT is unspecified, will
-    shift by 47.
+    `!caesar[N] <TEXT>` - Performs caesar shift with a left shift of N on given
+    text. If unspecified, will shift by 47.
     '''
-    command_args = command.arg.split() if command.has_arg() else []
-
-    arg_parser = argparse.ArgumentParser()
-    def usage_error(*args, **kwargs):
-        raise UsageSyntaxException()
-    arg_parser.error = usage_error  # type: ignore
-    arg_parser.add_argument('-n', default=47, type=int)
-    arg_parser.add_argument('text')
-
-    parsed_args = arg_parser.parse_args(command_args)
-    message = ''
-    for char in parsed_args.text:
+    text = message.get("text")
+    if message.get("subtype") == "bot_message" or text is None:
+        return
+    m = CAESAR_REGEX.match(text)
+    if not m:
+        return
+    n, msg = m.groups()
+    shift = 47 if n == "" else int(n)
+    result = ""
+    for c in msg:
         # 32 (SPACE) to 126 (~)
         # Get ascii code - 32. This makes SPACE the equivalent of 0
         # + n. Add caesar shift
         # mod 94 (from 126-32=94). This prevents overflow
         # + 32. Changes back (so SPACE is back to 32 instead of 0)
-        char_code = ord(char) - 32 + parsed_args.n
+        char_code = ord(c) - 32 + shift
         char_code = ((char_code % 94) + 94) % 94
         char_code += 32
-        message += chr(char_code)
-    bot.post_message(command.channel_id, message)
+        result += chr(char_code)
+    bot.post_message(message['channel'], result)
