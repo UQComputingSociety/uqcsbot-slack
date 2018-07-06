dfa309b5dfe3ecac67122e9bcbd70cc86bb4d5bd
jam <jack.d.caperon@gmail.com>
2018-07-06 16:07:46 +0800

    Removed unneeded codecs dependency, removed unused vars

--- test/test_umart.py
+++ test/test_umart.py
@@ -3,28 +3,30 @@ Tests for umart.py
 """
 from test.conftest import MockUQCSBot, TEST_CHANNEL_ID
 from unittest.mock import patch
-import codecs
 
 NO_QUERY_MESSAGE = "You can't look for nothing. `!umart <QUERY>`"
 NO_RESULTS_MESSAGE = "I can't find anything. Try `!umart <SOMETHING NOT AS SPECIFIC>`"
 ERROR_MESSAGE = "I tried to get the things but alas I could not. Error with HTTP Request."
 
-TEST_URL = "https://www.umart.com.au/umart1/pro/products_list_searchnew_min.phtml?search=HDD&bid=2"
-
 
 def get_test_message():
-    f = codecs.open("test/umart_test_message.txt", "r")
+    f = open("test/umart_test_message.txt", "r")
     return f.read()
-
-# This method will be used to replace the requests response
 
 
 def mocked_html_get(*args, **kwargs):
-    f = codecs.open("test/umart_products_list_search.html", "r")
+    """
+    This method will be used to replace the requests response
+    Returns locally stored HTML that represents a typically scraped response.
+    """
+    f = open("test/umart_products_list_search.html", "r")
     return f.read()
 
 
 def test_umart_no_query(uqcsbot: MockUQCSBot):
+    """
+
+    """
     uqcsbot.post_message(TEST_CHANNEL_ID, "!umart")
     messages = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
     assert len(messages) == 2
