77b1e7c77ed16321fba82fd2b5e9a51eb1640248
mcdee <mitchmcdee@gmail.com>
2018-08-11 13:39:06 +1000

    caesar: converted to argparse

--- uqcsbot/scripts/caesar.py
+++ uqcsbot/scripts/caesar.py
@@ -1,32 +1,28 @@
-import re
-from uqcsbot import bot
+import argparse
+import string
+from uqcsbot import bot, Command
+from uqcsbot.utils.command_utils import UsageSyntaxException
 
-CAESAR_REGEX = re.compile('!caesar(|-?\d+) (.+)')
 
-
-@bot.on('message')
-def handle_caesar(message: dict):
+@bot.on_command('caesar')
+def handle_caesar(command: Command):
     '''
-    `!caesar[N] <TEXT>` - Performs caesar shift with a left shift of N on given
-    text. If unspecified, will shift by 47.
+    `!caesar [<-n> <NUM_SHIFT>] <TEXT>` - Performs caesar shift with a left
+    shift of NUM_SHIFT on the given text. If NUM_SHIFT is unspecified, will
+    shift by 47.
     '''
-    text = message.get("text")
-    if message.get("subtype") == "bot_message" or text is None:
-        return
-    m = CAESAR_REGEX.match(text)
-    if not m:
-        return
-    n, msg = m.groups()
-    shift = 47 if n == "" else int(n)
-    result = ""
-    for c in msg:
-        # 32 (SPACE) to 126 (~)
-        # Get ascii code - 32. This makes SPACE the equivalent of 0
-        # + n. Add caesar shift
-        # mod 94 (from 126-32=94). This prevents overflow
-        # + 32. Changes back (so SPACE is back to 32 instead of 0)
-        char_code = ord(c) - 32 + shift
-        char_code = ((char_code % 94) + 94) % 94
-        char_code += 32
-        result += chr(char_code)
-    bot.post_message(message['channel'], result)
+    command_args = command.arg.split() if command.has_arg() else []
+
+    arg_parser = argparse.ArgumentParser()
+    def usage_error(*args, **kwargs):
+        raise UsageSyntaxException()
+    arg_parser.error = usage_error  # type: ignore
+    arg_parser.add_argument('-n', default=47, type=int)
+    arg_parser.add_argument('text')
+
+    parsed_args = arg_parser.parse_args(command_args)
+    printable_text = string.printable
+    shift_point = parsed_args.n % len(printable_text)
+    shifted_text = printable_text[shift_point:] + printable_text[:shift_point]
+    trans_table = str.maketrans(printable_text, shifted_text)
+    bot.post_message(command.channel_id, parsed_args.text.translate(trans_table))
