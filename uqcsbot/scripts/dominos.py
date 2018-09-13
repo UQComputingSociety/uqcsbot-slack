19a2eeccdebc8b57bd79386051acd7f6d467fba3
Leo Orpilla III <leoorpilla3@gmail.com>
2018-09-13 22:20:26 +1000

    Add argparse and keyword-filtering

--- uqcsbot/scripts/dominos.py
+++ uqcsbot/scripts/dominos.py
@@ -1,3 +1,4 @@
+import argparse
 from uqcsbot import bot, Command
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -23,25 +24,42 @@ class Coupon:
         except:
             return True
 
+    def keyword_matches(self, keyword: str) -> bool:
+        return keyword.lower() in self.description.lower()
+
 @bot.on_command("dominos")
 @loading_status
 def handle_dominos(command: Command):
     '''
-    `!dominos [N]` - Returns a list of dominos coupons (default: 5 | max: 10)
+    `!dominos [--num] N [--expiry] <KEYWORDS>` - Returns a list of dominos coupons (default: 5 | max: 10)
     '''
-    command_args = command.arg.strip() if command.has_arg() else 5
-    try:
-        coupons_amount = max(int(command_args), MAX_COUPONS)
-    except:
-        raise UsageSyntaxException()
+    command_args = command.arg.split() if command.has_arg() else []
 
-    coupons = get_coupons(int(coupons_amount))
+    parser = argparse.ArgumentParser()
+    def usage_error(*args, **kwargs):
+        raise UsageSyntaxException()
+    parser.error = usage_error
+    parser.add_argument('-n', '--num', default=5, type=int)
+    parser.add_argument('-e', '--expiry', action='store_true')
+    parser.add_argument('keywords', nargs='*')
+
+    args = parser.parse_args(command_args)
+    coupons_amount = min(args.num, MAX_COUPONS)
+    coupons = get_coupons(coupons_amount, args.expiry, args.keywords)
+
     message = ""
     for coupon in coupons:
         message += f"Code: *{coupon.code}* - {coupon.description}\n"
     bot.post_message(command.channel_id, message)
 
-def get_coupons(n:int) -> list:
+def filter_coupons(coupons: list, keywords: list) -> list:
+    '''
+    Filters coupons iff a keyword is found in the description.
+    '''
+    return [coupon for coupon in coupons if \
+            any(coupon.keyword_matches(keyword) for keyword in keywords)]
+        
+def get_coupons(n: int, ignore_expiry: bool, keywords: list) -> list:
     '''
     Returns a list of n Coupons
     '''
@@ -52,8 +70,12 @@ def get_coupons(n:int) -> list:
     
     coupons = get_coupons_from_page(coupon_page)
 
-    valid_coupons = [coupon for coupon in coupons if coupon.is_valid()]
-    return valid_coupons[:n]
+    if not ignore_expiry:
+        coupons = [coupon for coupon in coupons if coupon.is_valid()]
+
+    if keywords:
+        coupons = filter_coupons(coupons, keywords)
+    return coupons[:n]
 
 def get_coupons_from_page(coupon_page: bytes) -> list:
     '''
