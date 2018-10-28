9749f745d7a8d063444386af1d31bf2ff3fcbf3a
nicklambourne <ndl93@live.com>
2018-10-29 01:42:43 +1000

    addressed feedback

--- uqcsbot/scripts/xkcd.py
+++ uqcsbot/scripts/xkcd.py
@@ -8,7 +8,7 @@ from uqcsbot.utils.command_utils import loading_status
 # HTTP Endpoints
 XKCD_BASE_URL = "https://xkcd.com/"
 XKCD_RSS_URL = "https://xkcd.com/rss.xml"
-RELEVANT_XKCD_URL = 'https://relevantxkcd.appspot.com/process?action=xkcd&query='
+RELEVANT_XKCD_URL = 'https://relevantxkcd.appspot.com/process'
 
 
 def get_by_id(comic_number: int) -> str:
@@ -21,22 +21,24 @@ def get_by_id(comic_number: int) -> str:
         return "Invalid xkcd ID, it must be a positive integer."
     url = f"{XKCD_BASE_URL}{str(comic_number)}"
     response = requests.get(url)
-    if response.status_code == 200:
-        return url
-    else:
+    if response.status_code != 200:
         return "Could not retrieve an xkcd with that ID (are there even that many?)"
+    return url
 
 
-def get_by_search_phrase(phrase: str) -> str:
+def get_by_search_phrase(search_phrase: str) -> str:
     """
     Uses the site relevantxkcd.appspot.com to identify the most appropriate xkcd comic
     based on the phrase provided.
-    :param phrase: the phrase to find an xkcd comic related to.
+    :param search_phrase: the phrase to find an xkcd comic related to.
     :return: the URL of the most relevant comic for that search phrase.
     """
-    url = f"{RELEVANT_XKCD_URL}{quote(phrase)}"
-    response = requests.get(url)
+    params = {"action": "xkcd",
+              "query": quote(search_phrase)}
+    response = requests.get(RELEVANT_XKCD_URL, params=params)
+    # Response consists of a newline delimited list, with two irrelevant first parameters
     relevant_comics = response.content.decode().split("\n")[2:]
+    # Each line consists of "comic_id image_url"
     best_response = relevant_comics[0].split(" ")
     comic_number = int(best_response[0])
     return get_by_id(comic_number)
@@ -52,6 +54,20 @@ def get_latest() -> str:
     return latest
 
 
+def is_id(argument: str) -> bool:
+    """
+    Determines whether the given argument is a valid id (i.e. an integer).
+    :param argument: the string argument to evaluate
+    :return: true if the argument can be evaluated as an interger, false otherwise
+    """
+    try:
+        int(argument)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
 @bot.on_command('xkcd')
 @loading_status
 def handle_xkcd(command: Command) -> None:
@@ -62,10 +78,10 @@ def handle_xkcd(command: Command) -> None:
     """
     if command.has_arg():
         argument = command.arg
-        try:
+        if is_id(argument):
             comic_number = int(argument)
             response = get_by_id(comic_number)
-        except ValueError:
+        else:
             response = get_by_search_phrase(command.arg)
     else:
         response = get_latest()
