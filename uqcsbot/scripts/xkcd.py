204419e9f77df0058cf852d1651a0e74ac7357f4
Paul-Haley <paul.haley@bigpond.com>
2018-12-29 18:26:48 +1000

    #399 XKCD wrong day catch

--- uqcsbot/scripts/xkcd.py
+++ uqcsbot/scripts/xkcd.py
@@ -108,8 +108,10 @@ def new_xkcd() -> None:
         message = "Its Monday, 4 days till Friday; heres the "
     elif (day == 2): #Wednesday
         message = "Half way through the week, time for the "
-    else: #Friday
+    elif (day == 4): #Friday
         message = ":musical_note: It's Friday, Friday\nGotta get down on Friday\nEverybody's lookin' forward to the "
+    else:
+        message = "@pah It is day " + str(day) + ", please fix me... Here is the "
     message = message + "latest XKCD comic "
 
     general = bot.channels.get("general")
