b8233b8ab1b3cc26866bbe3f1d7a8874a79a777c
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2019-04-30 22:46:26 +1000

    Tests now use local data

--- test/test_weather.py
+++ test/test_weather.py
@@ -1,6 +1,23 @@
 from test.conftest import MockUQCSBot, TEST_CHANNEL_ID
+from unittest.mock import patch
+import xml.etree.ElementTree as ET
 
 
+def mocked_xml_get(*args, **kwargs):
+    """
+    This method will be used to replace the requests response
+    Returns locally stored XML Queensland forecasts from 2019.
+    """
+    source = {"NSW": "IDN11060", "ACT": "IDN11060", "NT": "IDD10207", "QLD": "IDQ11295", "SA": "IDS10044", "TAS": "IDT16710", "VIC": "IDV10753", "WA": "IDW14199"}
+    try:
+        data = open("test/bom_{}.xml".format(source[args[0]]))
+        root = ET.fromstring(data.read())
+    except:
+        return None
+    return root
+
+
+@patch("uqcsbot.scripts.weather.get_xml", new=mocked_xml_get)
 def test_brisbane(uqcsbot: MockUQCSBot):
     """
     Tests all varients !weather that give today's Brisbane weather
@@ -11,9 +28,9 @@ def test_brisbane(uqcsbot: MockUQCSBot):
     uqcsbot.post_message(TEST_CHANNEL_ID, '!weather 0')
     uqcsbot.post_message(TEST_CHANNEL_ID, '!weather Brisbane 0')
     uqcsbot.post_message(TEST_CHANNEL_ID, '!weather QLD Brisbane 0')
-    
+
     messages = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
-    for i in range(len(messages)-1,-1,-1):
+    for i in range(len(messages)-1, -1, -1):
         if messages[i]['text'].startswith("!weather"):
             messages.pop(i)
 
@@ -23,14 +40,18 @@ def test_brisbane(uqcsbot: MockUQCSBot):
 
     assert messages[0]['text'].split("\r\n")[0] == "*Today's Weather Forcast For Brisbane*"
 
+
+@patch("uqcsbot.scripts.weather.get_xml", new=mocked_xml_get)
 def test_tomorrow(uqcsbot: MockUQCSBot):
     """
-    Test !weather for tomorrows's Esk weather
+    Test !weather for tomorrow's Esk weather
     """
     uqcsbot.post_message(TEST_CHANNEL_ID, '!weather Esk 1')
     messages = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
     assert messages[-1]['text'].split("\r\n")[0] == "*Tomorrow's Weather Forcast For Esk*"
 
+
+@patch("uqcsbot.scripts.weather.get_xml", new=mocked_xml_get)
 def test_location(uqcsbot: MockUQCSBot):
     """
     Test !weather for an multi word interstate location
@@ -39,6 +60,8 @@ def test_location(uqcsbot: MockUQCSBot):
     messages = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
     assert messages[-1]['text'].split("\r\n")[0] == "*Today's Weather Forcast For Coffs Harbour*"
 
+
+@patch("uqcsbot.scripts.weather.get_xml", new=mocked_xml_get)
 def test_error(uqcsbot: MockUQCSBot):
     """
     Test !weather for error messages
@@ -46,7 +69,7 @@ def test_error(uqcsbot: MockUQCSBot):
     uqcsbot.post_message(TEST_CHANNEL_ID, '!weather nowhere')
     messages = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
     assert messages[-1]['text'] == "Location Not Found"
-    
+
     uqcsbot.post_message(TEST_CHANNEL_ID, '!weather -1')
     messages = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
     assert messages[-1]['text'] == "No Forecast Available For That Day"
--- uqcsbot/scripts/weather.py
+++ uqcsbot/scripts/weather.py
@@ -3,6 +3,16 @@ from urllib.request import urlopen
 import xml.etree.ElementTree as ET
 import datetime
 
+def get_xml(state: str):
+    source = {"NSW": "IDN11060", "ACT": "IDN11060", "NT": "IDD10207", "QLD": "IDQ11295", "SA": "IDS10044", "TAS": "IDT16710", "VIC": "IDV10753", "WA": "IDW14199"}
+    try:
+        data = urlopen("ftp://ftp.bom.gov.au/anon/gen/fwo/{}.xml".format(source[state]))
+        root = ET.fromstring(data.read())
+    except:
+        return None
+    return root
+    
+
 @bot.on_command('weather')
 def handle_weather(command: Command):
     """
@@ -11,12 +21,14 @@ def handle_weather(command: Command):
     """
 
     arguments = command.arg.split(" ") if command.has_arg() else []
-    
+
+    # get number of days into the future
     if arguments and arguments[-1].lstrip('-+').isnumeric():
         future = int(arguments.pop())
     else:
         future = 0
-    
+
+    # get location
     if arguments:
         if arguments[0].upper() in ["NSW", "ACT", "NT", "QLD", "SA", "TAS", "VIC", "WA"]:
             state = arguments.pop(0).upper()
@@ -27,14 +39,13 @@ def handle_weather(command: Command):
         state = "QLD"
         location = "Brisbane"
 
-    source = {"NSW": "IDN11060", "ACT": "IDN11060", "NT": "IDD10207", "QLD": "IDQ11295", "SA": "IDS10044", "TAS": "IDT16710", "VIC": "IDV10753", "WA": "IDW14199"}
-    try:
-        data = urlopen("ftp://ftp.bom.gov.au/anon/gen/fwo/{}.xml".format(source[state]))
-        root = ET.fromstring(data.read())
-    except:
+    # read BOM data
+    root = get_xml(state)
+    if root is None:
         bot.post_message(command.channel_id, "Could Not Retrieve BOM Data")
         return
-        
+
+    # find forecast
     node = root.find(".//area[@description='{}']".format(location))
     if node is None:
         bot.post_message(command.channel_id, "Location Not Found")
@@ -46,13 +57,15 @@ def handle_weather(command: Command):
     if node is None:
         bot.post_message(command.channel_id, "No Forecast Available For That Day")
         return
-    
+
+    # write day name, "today" or "tomorrow"
     forcast_date = datetime.datetime.strptime(node.get('start-time-local'), "%Y-%m-%dT%H:%M:%S%z").date()
     today_date = datetime.datetime.now().date()
     date_delta = (forcast_date - today_date).days
     date_name = "Today" if date_delta == 0 else "Tomorrow" if date_delta == 1 else forcast_date.strftime("%A")
     response = "*{}'s Weather Forcast For {}*".format(date_name, location)
 
+    # write overall forecast
     icon = node.find(".//element[@type='forecast_icon_code']")
     if icon is not None:
         icon = ["", "sunny", "clear", "partly-cloudy", "cloudy", "", "haze", "", "light-rain", "wind", "fog", "showers", "rain", "dust", "frost", "snow", "storm", "light-showers", "heavy-showers", "tropicalcyclone"][int(icon.text)]
@@ -60,7 +73,8 @@ def handle_weather(command: Command):
     descrip = node.find(".//text[@type='precis']")
     if descrip is not None:
         response += "\r\n{} {} {}".format(icon, descrip.text, icon)
-    
+
+    # write temperature
     temp_min = node.find(".//element[@type='air_temperature_minimum']")
     temp_max = node.find(".//element[@type='air_temperature_maximum']")
     if temp_min is not None and temp_max is not None:
@@ -69,7 +83,8 @@ def handle_weather(command: Command):
         response += "\r\nMinimum Temperature: {}ºC".format(temp_min.text)
     elif temp_max is not None:
         response += "\r\nMaximum Temperature: {}ºC".format(temp_max.text)
-    
+
+    # write precipitation
     rain_range = node.find(".//element[@type='precipitation_range']")
     precip_prob = node.find(".//text[@type='probability_of_precipitation']")
     if rain_range is not None and precip_prob is not None:
@@ -77,4 +92,5 @@ def handle_weather(command: Command):
     elif precip_prob is not None:
         response += "\r\n{} Chance of Precipitation".format(precip_prob.text)
 
+    # post
     bot.post_message(command.channel_id, response)
