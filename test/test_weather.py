2178f9fdfe7020f872015c48e54f208e3a9080a1
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2019-05-02 06:01:34 +1000

    Update test_weather.py
    Original script only worked on creation date

--- test/test_weather.py
+++ test/test_weather.py
@@ -1,9 +1,10 @@
 from test.conftest import MockUQCSBot, TEST_CHANNEL_ID
 from unittest.mock import patch
 import xml.etree.ElementTree as ET
+import datetime
 
 
-def mocked_xml_get(*args, **kwargs):
+def mocked_xml_get(state):
     """
     This method will be used to replace the requests response
     Returns locally stored XML Queensland forecasts from 2019.
@@ -11,14 +12,39 @@ def mocked_xml_get(*args, **kwargs):
     source = {"NSW": "IDN11060", "ACT": "IDN11060", "NT": "IDD10207", "QLD": "IDQ11295",
               "SA": "IDS10044", "TAS": "IDT16710", "VIC": "IDV10753", "WA": "IDW14199"}
     try:
-        data = open("test/bom_{}.xml".format(source[args[0]]))
+        data = open("test/bom_{}.xml".format(source[state]))
         root = ET.fromstring(data.read())
     except Exception:
         return None
     return root
 
 
+def mocked_response_header(node: ET.Element, location: str):
+    """
+    This method will be used to replace the header response
+    Returns the header for 30th Aprl, 2019.
+    """
+    # write day name, "today" or "tomorrow"
+    forcast_date = datetime.datetime.strptime(
+        "".join(node.get('start-time-local')
+                .rsplit(":", 1)), "%Y-%m-%dT%H:%M:%S%z"
+        ).date()
+    today_date = datetime.date(2019, 4, 30)
+    date_delta = (forcast_date - today_date).days
+    if date_delta == 0:
+        date_name = "Today"
+    elif date_delta == 1:
+        date_name = "Tomorrow"
+    elif date_delta == -1:
+        # can happen during the witching hours
+        date_name = "Yesterday"
+    else:
+        date_name = forcast_date.strftime("%A")
+    return "*{}'s Weather Forcast For {}*".format(date_name, location)
+
+
 @patch("uqcsbot.scripts.weather.get_xml", new=mocked_xml_get)
+@patch("uqcsbot.scripts.weather.response_header", new=mocked_response_header)
 def test_brisbane(uqcsbot: MockUQCSBot):
     """
     Tests all varients !weather that give today's Brisbane weather
@@ -43,6 +69,7 @@ def test_brisbane(uqcsbot: MockUQCSBot):
 
 
 @patch("uqcsbot.scripts.weather.get_xml", new=mocked_xml_get)
+@patch("uqcsbot.scripts.weather.response_header", new=mocked_response_header)
 def test_tomorrow(uqcsbot: MockUQCSBot):
     """
     Test !weather for tomorrow's Esk weather
@@ -53,6 +80,7 @@ def test_tomorrow(uqcsbot: MockUQCSBot):
 
 
 @patch("uqcsbot.scripts.weather.get_xml", new=mocked_xml_get)
+@patch("uqcsbot.scripts.weather.response_header", new=mocked_response_header)
 def test_location(uqcsbot: MockUQCSBot):
     """
     Test !weather for an multi word interstate location
@@ -63,6 +91,7 @@ def test_location(uqcsbot: MockUQCSBot):
 
 
 @patch("uqcsbot.scripts.weather.get_xml", new=mocked_xml_get)
+@patch("uqcsbot.scripts.weather.response_header", new=mocked_response_header)
 def test_error(uqcsbot: MockUQCSBot):
     """
     Test !weather for error messages
