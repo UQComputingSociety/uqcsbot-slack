19c159cdf7c00683899f4730a059724874bf312e
Joseph Garrone <josephgarrone@gmail.com>
2019-11-17 14:15:45 +1000

    Fixed water command so that listing commands work properly

--- uqcsbot/scripts/water.py
+++ uqcsbot/scripts/water.py
@@ -35,36 +35,37 @@ def handle_water(command: Command):
 
     # Print the list of regions
     if words[0].lower() in LIST_COMMAND:
+        response.append("Available regions:")
         for region in REGIONS:
-            response.append(f">{region} (aliases: {', '.join(REGIONS[region]['aliases'])}")
-
-    # Print the info for a specific region
-    if words[0].lower() in REGIONS or words[0].lower() in [alias for region in REGIONS for alias in REGIONS[region]['aliases']]:
-        actual_region = words[0].lower()
-        name = words[0]
-        if words[0].lower() not in REGIONS:    
-            for region in REGIONS:
-                if words[0].lower() in REGIONS[region]['aliases']:
-                    actual_region = region
-                    name = region
-
-        if actual_region == 'seq':
-            http_response = requests.get(REGIONS[actual_region]['url'])
-            html = BeautifulSoup(http_response.content, 'html.parser')
-
-            maximum_prev_sibling = html.find("div", string='Full supply capacity')
-            maximum_reading = maximum_prev_sibling.find_next_sibling("div")
-            current_prev_sibling = html.find("div", string='Current capacity')
-            current_reading = current_prev_sibling.find_next_sibling("div")
-
-            maximum = int("".join(list(filter(str.isdigit, maximum_reading.get_text()))))
-            current = int("".join(list(filter(str.isdigit, current_reading.get_text()))))
-            percent = (1.0 * current / maximum) * 100.0
-
-            response.append(f"{name} is at *{percent:3.2f}%* ({current:,}ML of {maximum:,}ML)")
-        else:
-            response.append(f"No region or alias found matching '{words[0]}'")    
+            response.append(f">{region} (aliases: {', '.join(REGIONS[region]['aliases'])})")
     else:
-        response.append(f"No region or alias found matching '{words[0]}'")
+        # Print the info for a specific region
+        if words[0].lower() in REGIONS or words[0].lower() in [alias for region in REGIONS for alias in REGIONS[region]['aliases']]:
+            actual_region = words[0].lower()
+            name = words[0]
+            if words[0].lower() not in REGIONS:    
+                for region in REGIONS:
+                    if words[0].lower() in REGIONS[region]['aliases']:
+                        actual_region = region
+                        name = region
+
+            if actual_region == 'seq':
+                http_response = requests.get(REGIONS[actual_region]['url'])
+                html = BeautifulSoup(http_response.content, 'html.parser')
+
+                maximum_prev_sibling = html.find("div", string='Full supply capacity')
+                maximum_reading = maximum_prev_sibling.find_next_sibling("div")
+                current_prev_sibling = html.find("div", string='Current capacity')
+                current_reading = current_prev_sibling.find_next_sibling("div")
+
+                maximum = int("".join(list(filter(str.isdigit, maximum_reading.get_text()))))
+                current = int("".join(list(filter(str.isdigit, current_reading.get_text()))))
+                percent = (1.0 * current / maximum) * 100.0
+
+                response.append(f"{name} is at *{percent:3.2f}%* ({current:,}ML of {maximum:,}ML)")
+            else:
+                response.append(f"No region or alias found matching '{words[0]}'")    
+        else:
+            response.append(f"No region or alias found matching '{words[0]}'")
 
     bot.post_message(command.channel_id, '\r\n'.join(response))
