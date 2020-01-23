f96d6f277cd59b6fd439bd5bdb69a9c1b0495304
nicklambourne <ndl93@live.com>
2020-01-23 21:53:29 +1100

    reordered messages

--- uqcsbot/scripts/welcome.py
+++ uqcsbot/scripts/welcome.py
@@ -6,7 +6,6 @@ import time
 
 MEMBER_MILESTONE = 50  # Number of members between posting a celebration
 MESSAGE_PAUSE = 2.5   # Number of seconds between sending bot messages
-LONG_PAUSE = 5   # Number of seconds before follow-up (profile) message is sent
 SLACK_DOWNLOAD_GUIDE = "https://slack.com/intl/en-au/help/categories/360000049043-Getting-" \
                        "Started#download-the-slack-app"
 UQCSBOT_REPO = "https://github.com/UQComputingSociety/uqcsbot"
@@ -16,35 +15,33 @@ WELCOME_MESSAGES = [    # Welcome messages sent to new members
     "Hey there! Welcome to the UQCS Slack!",
     "This is the first time I've seen you, so you're probably new here.",
     f"I'm UQCSbot, your friendly (<{UQCSBOT_REPO}|open source>) robot helper!",
+    "You can type `help` here, or `!help` anywhere else to find out what I can do!",
     "We've got a bunch of generic channels (e.g. <#C0D0BEYPM|banter>,"
-    + " <#C0DKX7NGP|games>, <#CB2K0Q09K|adulting>) along with many subject-specific"
-    + " ones (e.g <#C0MAN4BRS|csse1001>, <#C0Q2KTCK1|math1051>, <#C0DKSDGLE|csse2310>).",
+    " <#C0DKX7NGP|games>, <#CB2K0Q09K|adulting>) along with many subject-specific"
+    " ones (e.g <#C0MAN4BRS|csse1001>, <#C0Q2KTCK1|math1051>, <#C0DKSDGLE|csse2310>).",
     "To find and join a channel, tap on the channels header in the sidebar.",
     "The UQCS Slack is a friendly community and we have a code of conduct in place to ensure our "
-    + "members' well-being and safety. You can view a copy of this code of conduct here:"
-    + "\n>https://github.com/UQComputingSociety/code-of-conduct",
-    "UQCS elects a leadership committee every year who also serve as our friendly admins. This "
-    "year's committee consists of:"
-    + "\n>Madhav (<@UFB9R5QFM>)\n>Matthew (<@U8JN3NR1C>)\n>Bradley (<@U4AJ0EDE3>)"
-    + "\n>Sanni (<@UM55HGLUT>)\n>Jack (<@U29STGM63>)"
-    + "\n>James (<@U1H1R3NGK>)\n>Kenton (<@U9LMBPJG5>)"
-    + "\n>Nick (<@U0LG4V4NN>)\n>Olivia (<@UA25BSPGT>)"
-    + "\n>Raghav (<@U73H0R0QH>)\n>Brian (<@UCYC0TKMM>)",
-    "If you have any questions about UQCS, or need to get in touch, please reach out to them.",
-    "For a list of upcoming events check out the <#C0D0G52PP|events> channel and use the command "
-    "`!events`.",
-    "Type `help` here, or `!help` anywhere else to find out what I can do!",
-    f"Be sure to download the Slack <https://slack.com/downloads|desktop> and "
+    "members' well-being and safety. You can view a copy of this code of conduct here:"
+    "\n>https://github.com/UQComputingSociety/code-of-conduct",
+    "UQCS elects a leadership committee every year who also serve as our friendly Slack admins. "
+    "This year's committee consists of:\n"
+    ">Madhav (<@UFB9R5QFM>), Matthew (<@U8JN3NR1C>), Bradley (<@U4AJ0EDE3>)"
+    "Sanni (<@UM55HGLUT>), Jack (<@U29STGM63>), James (<@U1H1R3NGK>), Kenton (<@U9LMBPJG5>),"
+    "Nick (<@U0LG4V4NN>), Olivia (<@UA25BSPGT>), Raghav (<@U73H0R0QH>), "
+    "and Brian (<@UCYC0TKMM>)\n"
+    "If you have any questions or need to get in touch, please reach out to them.",
+    "We also hold heaps of events events during semester. For a list of upcoming events check "
+    "out the <#C0D0G52PP|events> channel and use the command `!events`.",
+    "Don't forget to let people know who you are! Choose a profile pic, set a "
+    "status and tell us what you're studying. These small touches help the UQCS "
+    "community understand who you are and, in turn, help you to more quickly "
+    "become an integral part of the society. If you need any help, check out "
+    f"this handy <{SLACK_PROFILE_GUIDE}|guide> or ask one admins for help!\n"
+    "Once that's all done, why not introduce yourself in <#C2R8W0YPJ|general>.",
+    "Be sure to download the Slack <https://slack.com/downloads|desktop> and "
     f"<{SLACK_DOWNLOAD_GUIDE}|mobile> apps as well, so you'll be able to catch any important "
-    f"announcements, and again, welcome to the UQ Computing Society :)"
+    "announcements, and again, welcome to the UQ Computing Society :)"
 ]
-
-PROFILE_MESSAGE = "Don't forget to let people know who you are! Choose a profile pic, set a " \
-                  "status and tell us what you're studying. These small touches help the UQCS " \
-                  "community understand who you are and, in turn, help you to more quickly " \
-                  "become an integral part of the society. If you need any help, check out " \
-                  f"this handy <{SLACK_PROFILE_GUIDE}|guide> or ask one admins for help!\n" \
-                  f"Once that's all done, why not introduce yourself in <#C2R8W0YPJ|general>."
 
 
 @bot.on("member_joined_channel")
@@ -84,7 +81,3 @@ def welcome(evt: dict):
     for message in WELCOME_MESSAGES:
         time.sleep(MESSAGE_PAUSE)
         bot.post_message(user.user_id, message)
-
-    time.sleep(LONG_PAUSE)
-
-    bot.post_message(user.user_id, PROFILE_MESSAGE)
