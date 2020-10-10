1b9e41aad36c112cf7c39d86aa25ea43a4c157f9
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2020-10-10 10:18:48 +1000

    Update test_help.py

--- test/test_help.py
+++ test/test_help.py
@@ -8,6 +8,6 @@ def test_help_help(uqcsbot: MockUQCSBot):
     uqcsbot.post_message(TEST_CHANNEL_ID, '!help help')
     messages = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
     assert len(messages) == 2
-    assert messages[-1]['text'] == (">>>`!help [COMMAND]` - Display the helper docstring"
+    assert messages[-1]['text'] == (">>> `!help [COMMAND]` - Display the helper docstring"
                                     + " for the given command. If unspecified, will return"
                                     + "the helper docstrings for all commands.")
