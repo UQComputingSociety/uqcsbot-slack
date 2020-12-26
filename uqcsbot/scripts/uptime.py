0dc59643669e79ebe4fbd33086e60a9971c41330
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2020-12-26 21:13:58 +1000

    Update uptime.py

--- uqcsbot/scripts/uptime.py
+++ uqcsbot/scripts/uptime.py
@@ -14,5 +14,7 @@ def handle_uptime(command: Command):
     message = ("The bot has been online"
                + f" {human(t, precision=(2 if t.total_seconds() >= 60 else 1), past_tense='{}'):s}"
                + (f" (`{round(t.total_seconds()):d}` seconds)" if t.total_seconds() >= 60 else "")
-               + f", since {bot.starttime.strftime('%H:%M:%S on %b %d'):s}.")
+               + f", since {bot.starttime.strftime('%H:%M:%S on %b %d'):s}"
+               # adds ordinal suffix
+               + f"{(lambda n: 'tsnrhtdd'[(n//10%10!=1)*(n%10<4)*n%10::4])(bot.starttime.day):s}.")
     command.reply_with(bot, message)
