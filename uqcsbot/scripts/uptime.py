528c93e9c294a8cea7c27e2f8965ba5a469950e9
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2020-12-26 21:17:42 +1000

    Update uptime.py

--- uqcsbot/scripts/uptime.py
+++ uqcsbot/scripts/uptime.py
@@ -11,7 +11,7 @@ def handle_uptime(command: Command):
     `!uptime` - displays the current uptime for UQCSBot
     """
     t = datetime.now() - bot.starttime
-    message = ("The bot has been online"
+    message = ("The bot has been online for"
                + f" {human(t, precision=(2 if t.total_seconds() >= 60 else 1), past_tense='{}'):s}"
                + (f" (`{round(t.total_seconds()):d}` seconds)" if t.total_seconds() >= 60 else "")
                + f", since {bot.starttime.strftime('%H:%M:%S on %b %d'):s}"
