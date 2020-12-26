716e0c85b8a92dc3bfb770a4edd22d7996656022
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2020-12-26 21:40:52 +1000

    Update test_uptime.py

--- test/test_uptime.py
+++ test/test_uptime.py
@@ -6,4 +6,6 @@ def test_uptime(uqcsbot: MockUQCSBot):
     test !uptime for response
     """
     uqcsbot.post_message(TEST_CHANNEL_ID, '!uptime')
-    assert len(uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])) == 2
+    messages = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
+    assert len(messages) == 2
+    assert messages[-1]["text"].startswith("The bot has been online for"
