d85446b2ca76f6699a6d04b8b6e1f7633e796c6a
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2020-12-26 21:04:40 +1000

    Update uptime.py

--- uqcsbot/scripts/uptime.py
+++ uqcsbot/scripts/uptime.py
@@ -1,21 +1,18 @@
 from uqcsbot import bot, Command
-from time import time, localtime, strftime
+from uqcsbot.utils.command_utils import loading_status
+from datetime import datetime
+from ago import human
 
 
 @bot.on_command("uptime")
+@loading_status
 def handle_uptime(command: Command):
     """
     `!uptime` - displays the current uptime for UQCSBot
     """
-    t = round(time() - bot.starttime)
-    s = t % 60
-    m = (t//60) % 60
-    h = (t//3600) % 24
-    d = (t//86400)
-    message = (f"`{t:d}`\n" +
-               (f"{d:d}d " if d else "")
-               + (f"{h:d}h " if d or h else "")
-               + (f"{m:d}m " if d or h or m else "")
-               + (f"{s:d}s " if d or h or m or s else "") + "\n" +
-               strftime("%H:%M:%S - %b %d", localtime(bot.starttime)))
+    t = datetime.now() - bot.starttime
+    message = ("The bot has been online"
+               + f" {human(t, precision=(2 if t.total_seconds() >= 60 else 1), past_tense='{}'):s}"
+               + (f" (`{round(t.total_seconds()):d}` seconds)" if t.total_seconds() >= 60 else "")
+               + f", since {bot.starttime.strftime('%H:%M:%S - %b %d'):s}")
     command.reply_with(bot, message)
