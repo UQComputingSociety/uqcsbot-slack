1946f2abff72cad843c02debae6b619383fbb845
Enrique Gonzalez Aretos <gonzalezaretos@gmail.com>
2021-03-16 14:02:55 +1000

    Fixed under-indentation on test file assertion

--- test/test_news.py
+++ test/test_news.py
@@ -24,15 +24,15 @@ def test_news(uqcsbot: MockUQCSBot):
     uqcsbot.post_message(TEST_CHANNEL_ID, "!news")
     message = uqcsbot.test_messages.get(TEST_CHANNEL_ID, [])
     assert(message[-1]['text'] ==
-        f"------------------------- Latest News from TechCrunch ---------------------------\n"
-        f"<http://feedproxy.google.com/~r/Techcrunch/~3/99Ndo-D1yGA/|"
-        f"Rising encrypted app Signal is down in China>\n\n"
-        f"<http://feedproxy.google.com/~r/Techcrunch/~3/CvyW_t3oxSQ/|"
-        f"China wants to dismantle Alibaba’s media empire: reports>\n\n"
-        f"<http://feedproxy.google.com/~r/Techcrunch/~3/EqeGTrnZEYs/|"
-        f"Bird to spend $150 million on European expansion plan>\n\n"
-        f"<http://feedproxy.google.com/~r/Techcrunch/~3/X98SDLR4A6Q/|"
-        f"Sherpa raises $8.5M to expand from conversational AI to B2B "
-        f"privacy-first federated learning services>\n\n"
-        f"<http://feedproxy.google.com/~r/Techcrunch/~3/SdwlDvQPZHU/|"
-        f"Daily Crunch: Stripe valued at $95B>\n\n")
+            "------------------------- Latest News from TechCrunch ---------------------------\n"
+            "<http://feedproxy.google.com/~r/Techcrunch/~3/99Ndo-D1yGA/|"
+            "Rising encrypted app Signal is down in China>\n\n"
+            "<http://feedproxy.google.com/~r/Techcrunch/~3/CvyW_t3oxSQ/|"
+            "China wants to dismantle Alibaba’s media empire: reports>\n\n"
+            "<http://feedproxy.google.com/~r/Techcrunch/~3/EqeGTrnZEYs/|"
+            "Bird to spend $150 million on European expansion plan>\n\n"
+            "<http://feedproxy.google.com/~r/Techcrunch/~3/X98SDLR4A6Q/|"
+            "Sherpa raises $8.5M to expand from conversational AI to B2B "
+            "privacy-first federated learning services>\n\n"
+            "<http://feedproxy.google.com/~r/Techcrunch/~3/SdwlDvQPZHU/|"
+            "Daily Crunch: Stripe valued at $95B>\n\n")
