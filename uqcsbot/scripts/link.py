0e8d43fb736e43b44797eb5a5abd2783b4b8798d
nicklambourne <ndl93@live.com>
2021-03-30 01:34:56 +1100

    seems to work XD

--- uqcsbot/scripts/link.py
+++ uqcsbot/scripts/link.py
@@ -42,14 +42,17 @@ def set_link_value(key: str, value: str, channel: str,
             return SetResult.NEEDS_OVERRIDE, exists.value
         exists.value = value
         session.commit()
+        session.close()
         return SetResult.OVERRIDE_SUCCESS, exists.value
     except NoResultFound:
         session.add(Link(key=key, channel=link_channel, value=value))
         session.commit()
+        session.close()
         return SetResult.NEW_LINK_SUCCESS, value
 
 
-def get_link_value(key: str, channel: str, global_flag: bool, channel_flag: bool) -> Optional[str]:
+def get_link_value(key: str, channel: str,
+                   global_flag: bool, channel_flag: bool) -> Tuple[Optional[str], Optional[str]]:
     """
     Gets the value associated with a given key (and optionally channel). If a channel association
     exists, this is returned, otherwise a global association is returned. If no association exists
@@ -59,39 +62,42 @@ def get_link_value(key: str, channel: str, global_flag: bool, channel_flag: bool
     :param channel: the name of the channel the lookup request was made from
     :param global_flag: required to be True if the global association is requested
     :param channel_flag: required to be True if the channel associate is requested
-    :return: the associated value if an association exists, else None
+    :return: the associated value if an association exists, else None, and the source
+    (global/channel) if any else None
     """
     session = bot.create_db_session()
     channel_match = session.query(Link).filter(Link.key == key,
                                                Link.channel == channel).one_or_none()
     global_match = session.query(Link).filter(Link.key == key,
                                               Link.channel == None).one_or_none()
+    session.close()
+
     if global_flag:
-        return global_match.value if global_match else None
+        return (global_match.value, "global") if global_match else None, None
 
     if channel_flag:
-        return channel_match.value if channel_match else None
+        return (channel_match.value, "channel") if channel_match else None, None
 
     if channel_match:
-        return global_match.value
+        return channel_match.value, "channel"
 
     if global_match:
-        return global_match.value
+        return global_match.value, "global"
 
-    return None
+    return None, None
 
 
 @bot.on_command('link')
 @loading_status
 def handle_link(command: Command) -> None:
     """
-    `!link [-c | -g | -o] <key> [value]` - Set and retrieve information in a key value store.
-    Links can be set to be channel specific or global. Links are set as global by default, and
-    channel specific links are retrieved by default unless overridden with the respective flag.
+    `!link [-c | -g] [-o] key [value [value ...]]` - Set and retrieve information in a key value
+    store. Links can be set to be channel specific or global. Links are set as global by default,
+    and channel specific links are retrieved by default unless overridden with the respective flag.
     """
     parser = ArgumentParser("!link", add_help=False)
     parser.add_argument("key", type=str, help="Lookup key")
-    parser.add_argument("value", type=str, help="Value to associate with key", nargs="+")
+    parser.add_argument("value", type=str, help="Value to associate with key", nargs="*")
     flag_group = parser.add_mutually_exclusive_group()
     flag_group.add_argument("-c", "--channel", action="store_true", dest="channel_flag",
                             help="Ensure a channel link is retrieved, or none is")
@@ -99,31 +105,42 @@ def handle_link(command: Command) -> None:
                             help="Ignore channel link and force retrieval of global")
     parser.add_argument("-o", "--override", action="store_true")
 
-    args = parser.parse_args(command.arg.split() if command.has_arg() else [])
-    channel = bot.channels.get(command.channel_id, use_cache=False)
+    try:
+        args = parser.parse_args(command.arg.split() if command.has_arg() else [])
+    except SystemExit:
+        # Incorrect Usage
+        return bot.post_message(command.channel_id, "",
+                                attachments=[Attachment(SectionBlock(str(parser.format_usage())),
+                                                        color=Color.YELLOW)._resolve()])
 
-    # Incorrect Usage
-    if not args.key:
-        bot.post_message(attachments=[Attachment(SectionBlock(parser.format_help()),
-                                                 color=Color.WARNING)._resolve()])
+    channel = bot.channels.get(command.channel_id)
+    if not channel:
+        attachment = Attachment(SectionBlock("Cannot find channel name, please try again."),
+                                color=Color.YELLOW)._resolve()
+        return bot.post_message(command.channel_id, attachments=[attachment])
+    else:
+        channel_name = bot.channels.get(command.channel_id).name
 
     # Retrieve a link
     if not args.value:
-        link_value = get_link_value(args.key, channel, args.global_flag, args.channel_flag)
-        response = f"{args.key}: {link_value}" if link_value else f"No link found"
-        color = Color.GOOD if link_value else Color.DANGER
-        attachment = Attachment(SectionBlock(response), color=color)
-        return bot.post_message(channel, attatchments=[attachment])
+        link_value, source = get_link_value(args.key, channel_name, args.global_flag,
+                                            args.channel_flag)
+        if_channel_flag = f" in channel `{channel_name}`" if args.channel_flag else ""
+        response = f"{args.key} ({source}): {link_value}" if link_value else \
+            f"No link found for key: `{args.key}` {if_channel_flag}"
+        color = Color.GREEN if link_value else Color.RED
+        attachment = Attachment(SectionBlock(response), color=color)._resolve()
+        return bot.post_message(command.channel_id, "", attachments=[attachment])
 
     # Set a link
     if args.key and args.value:
         result, current_value = set_link_value(key=args.key,
-                                               channel=channel,
-                                               value=args.value,
+                                               channel=channel_name,
+                                               value=" ".join(args.value),
                                                channel_flag=args.channel_flag,
                                                override=args.override)
-        color = Color.DANGER if result == SetResult.NEEDS_OVERRIDE else Color.GOOD
-        response = f"{result}: {args.key} ({channel if args.channel_flag else 'global'}) = " \
+        color = Color.YELLOW if result == SetResult.NEEDS_OVERRIDE else Color.GREEN
+        response = f"{args.key} ({channel_name if args.channel_flag else 'global'}) = " \
                    f"{current_value}"
-        attachment = Attachment(SectionBlock(response), color=color)
-        bot.post_message(channel, f"{result.value}:", attachments=[attachment])
+        attachment = Attachment(SectionBlock(response), color=color)._resolve()
+        bot.post_message(command.channel_id, f"{result.value}:", attachments=[attachment])
