1c03655f668c28cefc8dabdc6f15deef3979c8c2
James Dearlove <jamesdearlove@outlook.com>
2021-09-17 14:36:16 +1000

    Update deploy to include restart policy

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ docker pull ghcr.io/uqcomputingsociety/uqcsbot-slack:latest
 OLDCONTAINERS="$(docker ps -f label=uqcsbot-slack-ci -a -q)"
 docker run -d \
   --label uqcsbot-slack-ci \
+  --restart always \
   -e SLACK_BOT_TOKEN=${SLACK_BOT_TOKEN} \
   -e SLACK_USER_TOKEN=${SLACK_USER_TOKEN} \
   -e SLACK_VERIFICATION_TOKEN=${SLACK_VERIFICATION_TOKEN} \
